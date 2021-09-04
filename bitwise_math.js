9dbed6ad58cc3e8e3ab0c49a7b3eb229317164ba
RazyRabbit <razyrabbit@gmail.com>
2021-09-04 10:05:52 -0300

    Adds extension 'bitwise_math'

---
+++ bitwise_math.js
@@ -0,0 +1,122 @@
+
+
+class BitwiseMath {
+	getInfo() {
+		return {
+			id: "BitwiseMath",
+
+			color1: '#2dc4c4',
+			color2: '#2dc4b3',
+			color3: '#29a395',
+			
+			blocks: [
+				{
+					opcode: 'hasBitwiseMath',
+					blockType: Scratch.BlockType.BOOLEAN,
+					text: 'has BitwiseMath?'
+				},
+				{
+					opcode: 'bitwiseLeftShift',
+					blockType: Scratch.BlockType.REPORTER,
+					text: '[LEFT] << [RIGHT]',
+					arguments: {
+						LEFT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '2'
+						},
+						RIGHT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '1'
+						}
+					}
+				},
+				{
+					opcode: 'bitwiseRightShift',
+					blockType: Scratch.BlockType.REPORTER,
+					text: '[LEFT] >> [RIGHT]',
+					arguments: {
+						LEFT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '2'
+						},
+						RIGHT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '1'
+						}
+					}
+				},
+				{
+					opcode: 'bitwiseAnd',
+					blockType: Scratch.BlockType.REPORTER,
+					text: '[LEFT] & [RIGHT]',
+					arguments: {
+						LEFT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '3'
+						},
+						RIGHT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '1'
+						}
+					}
+				},
+				{
+					opcode: 'bitwiseOr',
+					blockType: Scratch.BlockType.REPORTER,
+					text: '[LEFT] | [RIGHT]',
+					arguments: {
+						LEFT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '1'
+						},
+						RIGHT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '0'
+						}
+					}
+				},
+				{
+					opcode: 'bitwiseXor',
+					blockType: Scratch.BlockType.REPORTER,
+					text: '[LEFT] ^ [RIGHT]',
+					arguments: {
+						LEFT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '1'
+						},
+						RIGHT: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: '1'
+						}
+					}
+				}
+			]
+		};
+	}
+	
+	hasBitwiseMath() {
+		return true;
+	}
+	
+	bitwiseLeftShift({LEFT, RIGHT}) {
+		return LEFT << RIGHT;
+	}
+	
+	bitwiseRightShift({LEFT, RIGHT}) {
+		return LEFT >> RIGHT;
+	}
+	
+	bitwiseAnd({LEFT, RIGHT}) {
+		return LEFT & RIGHT;
+	}
+	
+	bitwiseOr({LEFT, RIGHT}) {
+		return LEFT | RIGHT;
+	}
+	
+	bitwiseXor({LEFT, RIGHT}) {
+		return LEFT ^ RIGHT;
+	}
+}
+
+Scratch.extensions.register(new BitwiseMath());
