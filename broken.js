14e4fd890fa0c58e93b1af27e125c8ce4c692994
GarboMuffin <muffin@muffin.ink>
2022-09-06 21:38:42 -0500

    Create broken.js

---
+++ broken.js
@@ -0,0 +1,30 @@
+class Broken {
+  getInfo () {
+    return {
+      id: 'broken',
+      name: 'Broken Extension',
+      blocks: [
+        {
+          opcode: 'error',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'error'
+        },
+        {
+          opcode: 'returnUndefined',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'return undefined'
+        }
+      ]
+    };
+  }
+
+  error () {
+    throw new Error("This is an error :(");
+  }
+  
+  returnUndefined () {
+    
+  }
+}
+
+Scratch.extensions.register(new Broken());
