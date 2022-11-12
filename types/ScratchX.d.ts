09710614bb39515263990f6d2dc2eeb68c94256d
GarboMuffin <muffin@mailbox.org>
2022-11-12 02:22:00 -0600

    Add type definitions based on @turbowarp/types

---
+++ types/ScratchX.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @deprecated ScratchX is deprecated. Do not use in new extensions.
+ */
+declare namespace ScratchExtensions {
+  /**
+   * @deprecated ScratchX is deprecated. Do not use in new extensions.
+   */
+  function register(name: string, descriptor: unknown, extension: unknown): void;
+}
