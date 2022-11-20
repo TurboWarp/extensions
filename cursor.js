5a759574602ad78a5391d69688827398d327393c
GarboMuffin <muffin@mailbox.org>
2022-11-19 20:21:56 -0600

    cursor: Format code

--- cursor.js
+++ cursor.js
@@ -8,39 +8,48 @@
 
         getInfo() {
             return {
-                "id": "MouseCursor",
-                "name": "Mouse Cursor",
-                "blocks": [
-                    {
-                        "opcode": "SwitchCur",
-                        "blockType": "command",
-                        "text": "switch cursor to [cur]",
-                        "arguments": {
-                            "cur": {
-                                "type": "string",
-                                "defaultValue": "pointer",
-                                "menu": "cursors"
-                            },
-                        },
+              id: "MouseCursor",
+              name: "Mouse Cursor",
+              blocks: [
+                {
+                  opcode: "SwitchCur",
+                  blockType: "command",
+                  text: "switch cursor to [cur]",
+                  arguments: {
+                    cur: {
+                      type: "string",
+                      defaultValue: "pointer",
+                      menu: "cursors",
                     },
-                    {
-                        "opcode": "hide",
-                        "blockType": "command",
-                        "text": "hide cursor",
-                    },
-                    {
-                        "opcode": "GetCur",
-                        "blockType": "reporter",
-                        "text": "cursor",
-                    },
-                ],
+                  },
+                },
+                {
+                  opcode: "hide",
+                  blockType: "command",
+                  text: "hide cursor",
+                },
+                {
+                  opcode: "GetCur",
+                  blockType: "reporter",
+                  text: "cursor",
+                },
+              ],
 
-                "menus": {
-                    "cursors": {
-                        acceptReporters: true,
-                        items: [{ text: "default", value: "default" }, { text: "pointer", value: "pointer" }, { text: "crosshair", value: "crosshair" }, { text: "move", value: "move" }, { text: "text", value: "text" }, { text: "wait", value: "wait" }, { text: "progress", value: "progress" }, { text: "help", value: "help" }],
-                    }
-                }
+              menus: {
+                cursors: {
+                  acceptReporters: true,
+                  items: [
+                    {text: "default", value: "default"},
+                    {text: "pointer", value: "pointer"},
+                    {text: "crosshair", value: "crosshair"},
+                    {text: "move", value: "move"},
+                    {text: "text", value: "text"},
+                    {text: "wait", value: "wait"},
+                    {text: "progress", value: "progress"},
+                    {text: "help", value: "help"},
+                  ],
+                },
+              },
             };
         }
 
