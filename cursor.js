1a438eb15d18fdcf01fd8f1e086bd4e8bf2c7ba0
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-11-19 21:57:25 -0500

    Mouse Cursor Extension (#39)
    * Create cursor.js
    
    * remove reset cursor block
    
    * cursor: fix default cursor value before calling set
    
    * cursor: Use non-scratch-gui specific element target
    
    * cursor: Format code
    
    * cursor: Make type checker happy
    
    * Format code + better function names
    
    * Throw error when sandboxed,
    
    Co-authored-by: GarboMuffin <muffin@mailbox.org>

---
+++ cursor.js
@@ -0,0 +1,73 @@
+(function (Scratch) {
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('MouseCursor extension must be run unsandboxed');
+  }
+
+  class MouseCursor {
+    constructor() {
+      this.canvas = Scratch.renderer.canvas;
+    }
+
+    getInfo() {
+      return {
+        id: 'MouseCursor',
+        name: 'Mouse Cursor',
+        blocks: [
+          {
+            opcode: 'setCur',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'set cursor to [cur]',
+            arguments: {
+              cur: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'pointer',
+                menu: 'cursors',
+              },
+            },
+          },
+          {
+            opcode: 'hideCur',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'hide cursor',
+          },
+          {
+            opcode: 'getCur',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'cursor',
+          },
+        ],
+        menus: {
+          cursors: {
+            acceptReporters: true,
+            items: [
+              { text: 'default', value: 'default' },
+              { text: 'pointer', value: 'pointer' },
+              { text: 'crosshair', value: 'crosshair' },
+              { text: 'move', value: 'move' },
+              { text: 'text', value: 'text' },
+              { text: 'wait', value: 'wait' },
+              { text: 'progress', value: 'progress' },
+              { text: 'help', value: 'help' },
+            ],
+          },
+        },
+      };
+    }
+
+    setCur(args) {
+      this.canvas.style.cursor = args.cur;
+    }
+
+    hideCur() {
+      this.canvas.style.cursor = 'none';
+    }
+
+    getCur() {
+      return this.canvas.style.cursor || 'default';
+    }
+  }
+
+  Scratch.extensions.register(new MouseCursor());
+})(Scratch);
