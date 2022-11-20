8fbd2731184fd90882c0d9d8147042145fd4e652
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-11-19 21:48:14 -0500

    Format code + better function names

--- cursor.js
+++ cursor.js
@@ -1,70 +1,69 @@
 (function (Scratch) {
-    'use strict';
+  'use strict';
 
-    class MouseCursor {
-        constructor() {
-            this.canvas = Scratch.renderer.canvas;
-        }
-
-        getInfo() {
-            return {
-              id: "MouseCursor",
-              name: "Mouse Cursor",
-              blocks: [
-                {
-                  opcode: "SwitchCur",
-                  blockType: Scratch.BlockType.COMMAND,
-                  text: "switch cursor to [cur]",
-                  arguments: {
-                    cur: {
-                      type: Scratch.ArgumentType.STRING,
-                      defaultValue: "pointer",
-                      menu: "cursors",
-                    },
-                  },
-                },
-                {
-                  opcode: "hide",
-                  blockType: Scratch.BlockType.COMMAND,
-                  text: "hide cursor",
-                },
-                {
-                  opcode: "GetCur",
-                  blockType: Scratch.BlockType.REPORTER,
-                  text: "cursor",
-                },
-              ],
-
-              menus: {
-                cursors: {
-                  acceptReporters: true,
-                  items: [
-                    {text: "default", value: "default"},
-                    {text: "pointer", value: "pointer"},
-                    {text: "crosshair", value: "crosshair"},
-                    {text: "move", value: "move"},
-                    {text: "text", value: "text"},
-                    {text: "wait", value: "wait"},
-                    {text: "progress", value: "progress"},
-                    {text: "help", value: "help"},
-                  ],
-                },
-              },
-            };
-        }
-
-        SwitchCur({ cur }) {
-            this.canvas.style.cursor = cur;
-        }
-
-        hide() {
-            this.canvas.style.cursor = "none";
-        }
-
-        GetCur() {
-            return this.canvas.style.cursor || 'default';
-        }
+  class MouseCursor {
+    constructor() {
+      this.canvas = Scratch.renderer.canvas;
     }
 
-    Scratch.extensions.register(new MouseCursor());
+    getInfo() {
+      return {
+        id: 'mouseCursor',
+        name: 'Mouse Cursor',
+        blocks: [
+          {
+            opcode: 'setCur',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'set cursor to [cur]',
+            arguments: {
+              cur: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'pointer',
+                menu: 'cursors',
+              },
+            },
+          },
+          {
+            opcode: 'hideCur',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'hide cursor',
+          },
+          {
+            opcode: 'getCur',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'cursor',
+          },
+        ],
+        menus: {
+          cursors: {
+            acceptReporters: true,
+            items: [
+              { text: 'default', value: 'default' },
+              { text: 'pointer', value: 'pointer' },
+              { text: 'crosshair', value: 'crosshair' },
+              { text: 'move', value: 'move' },
+              { text: 'text', value: 'text' },
+              { text: 'wait', value: 'wait' },
+              { text: 'progress', value: 'progress' },
+              { text: 'help', value: 'help' },
+            ],
+          },
+        },
+      };
+    }
+
+    setCur({ cur }) {
+      this.canvas.style.cursor = cur;
+    }
+
+    hideCur() {
+      this.canvas.style.cursor = 'none';
+    }
+
+    getCur() {
+      return this.canvas.style.cursor || 'default';
+    }
+  }
+
+  Scratch.extensions.register(new MouseCursor());
 })(Scratch);
