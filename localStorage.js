2ac0823d017b86eec7685c68f7f5a3850f47f5e4
stio <98958235+StioStudio@users.noreply.github.com>
2022-12-23 19:22:13 +0100

    Update localStorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2023 StioStudio (Simon)
+ * Copyright 2023 StioStudio
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
