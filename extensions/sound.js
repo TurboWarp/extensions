8733a23e0aa76bd1fddcb6553bdc832e929996ff
GarboMuffin <muffin@mailbox.org>
2023-01-02 00:09:30 -0600

    Convert to spaces

--- extensions/sound.js
+++ extensions/sound.js
@@ -1,57 +1,57 @@
 (Scratch => {
-	'use strict';
+  'use strict';
 
-	class Sound {
-		getInfo() {
-			return {
-				// 'sound' would conflict with normal Scratch
-				id: 'notSound',
-				name: 'Sound',
-				blocks: [
-					{
-						opcode: 'play',
-						blockType: Scratch.BlockType.COMMAND,
-						text: 'Play sound from url:[path]',
-						arguments: {
-							path: {
-								type: Scratch.ArgumentType.STRING,
-								defaultValue: 'https://extensions.turbowarp.org/meow.mp3'
-							}
-						}
-					},
-					{
-						opcode: 'playUntilDone',
-						blockType: Scratch.BlockType.COMMAND,
-						text: 'Play sound from url:[path] until done',
-						arguments: {
-							path: {
-								type: Scratch.ArgumentType.STRING,
-								defaultValue: 'https://extensions.turbowarp.org/meow.mp3'
-							}
-						}
-					}
-				]
-			};
-		}
+  class Sound {
+    getInfo() {
+      return {
+        // 'sound' would conflict with normal Scratch
+        id: 'notSound',
+        name: 'Sound',
+        blocks: [
+          {
+            opcode: 'play',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Play sound from url:[path]',
+            arguments: {
+              path: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://extensions.turbowarp.org/meow.mp3'
+              }
+            }
+          },
+          {
+            opcode: 'playUntilDone',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Play sound from url:[path] until done',
+            arguments: {
+              path: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://extensions.turbowarp.org/meow.mp3'
+              }
+            }
+          }
+        ]
+      };
+    }
 
-		play({path}) {
-			const sound = new Audio(path);
-			sound.play();
-		}
+    play({path}) {
+      const sound = new Audio(path);
+      sound.play();
+    }
 
-		playUntilDone({path}) {
-			return new Promise(resolve => {
-				const sound = new Audio(path);
-				sound.play();
-				sound.onended = () => {
-					resolve();
-				};
-				sound.onerror = () => {
-					resolve();
-				};
-			});
-		}
-	}
+    playUntilDone({path}) {
+      return new Promise(resolve => {
+        const sound = new Audio(path);
+        sound.play();
+        sound.onended = () => {
+          resolve();
+        };
+        sound.onerror = () => {
+          resolve();
+        };
+      });
+    }
+  }
 
-	Scratch.extensions.register(new Sound());
+  Scratch.extensions.register(new Sound());
 })(Scratch);
