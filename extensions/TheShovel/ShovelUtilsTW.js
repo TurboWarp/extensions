5da0da0d99a6224f75d702997b9eb484afdb9579
TheShovel <68913917+TheShovel@users.noreply.github.com>
2023-02-12 20:52:48 +0200

    Apply suggestions from code review
    Co-authored-by: !Ryan <100989385+softedco@users.noreply.github.com>

--- extensions/TheShovel/ShovelUtilsTW.js
+++ extensions/TheShovel/ShovelUtilsTW.js
@@ -1,14 +1,14 @@
 console.log("ShovelUtils v1.1")
-tempImageLoad = null
-string = null;
-temp = null;
-tempVAR = null;
+var tempImageLoad = {};
+var string = '';
+var temp = {};
+var tempVAR = {};
 
 
 
 (function(Scratch) {
-  const vm = Scratch.vm;
   'use strict';
+  const vm = Scratch.vm;
   class ShovelUtils {
     getInfo () {
       return { 
@@ -237,15 +237,16 @@ setedtarget({NAME}){
  * @returns (Number) The brightness value (dark) 0 ... 255 (light)
  */
 brightnessByColor ({color}) {
-  var color = "" + color, isHEX = color.indexOf("#") == 0, isRGB = color.indexOf("rgb") == 0;
+  color = "" + color;
+  var isHEX = color.indexOf("#") == 0, isRGB = color.indexOf("rgb") == 0;
   if (isHEX) {
     const hasFullSpec = color.length == 7;
     var m = color.substr(1).match(hasFullSpec ? /(\S{2})/g : /(\S{1})/g);
     if (m) var r = parseInt(m[0] + (hasFullSpec ? '' : m[0]), 16), g = parseInt(m[1] + (hasFullSpec ? '' : m[1]), 16), b = parseInt(m[2] + (hasFullSpec ? '' : m[2]), 16);
   }
   if (isRGB) {
-    var m = color.match(/(\d+){3}/g);
-    if (m) var r = m[0], g = m[1], b = m[2];
+    m = color.match(/(\d+){3}/g);
+    if (m) r = m[0], g = m[1], b = m[2];
   }
   if (typeof r != "undefined") return ((r*299)+(g*587)+(b*114))/1000;
 }
