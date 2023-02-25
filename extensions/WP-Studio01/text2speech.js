7817d2e4bfb069fc1b9de87c39c5c9cc1f3e4778
HelloWorld <98379477+WP-Studio01@users.noreply.github.com>
2023-02-26 04:56:44 +0800

    Add another text to speech extension (#185)
    Uses the system TTS instead of Scratch's TTS

---
+++ extensions/WP-Studio01/text2speech.js
@@ -0,0 +1,40 @@
+(function(Scratch) {
+  'use strict';
+
+  class Tools {
+    getInfo () {
+      return {
+        id: 'wpstudio01tts',
+        name: 'System Text To Speech',
+        blocks: [
+          {
+            opcode: 'speak',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'speak [TEXT]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Hello'
+              }
+            }
+          }
+        ]
+      };
+    }
+
+    speak (args) {
+      return new Promise((resolve, reject) => {
+        const utterance = new SpeechSynthesisUtterance(args.TEXT);
+        utterance.onend = () => {
+          resolve();
+        };
+        utterance.onerror = () => {
+          reject(new Error('Utterance error'));
+        };
+        speechSynthesis.speak(utterance);
+      });
+    }
+  }
+
+  Scratch.extensions.register(new Tools());
+})(Scratch);
