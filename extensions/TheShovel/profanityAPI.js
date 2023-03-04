35fac0a517304c3f6e42008062f26bbd2ae59708
TheShovel <68913917+TheShovel@users.noreply.github.com>
2023-03-04 16:23:42 +0200

    The profanity extension, but using the api

---
+++ extensions/TheShovel/profanityAPI.js
@@ -0,0 +1,38 @@
+(function(Scratch) {
+    'use strict'
+    const vm = Scratch.vm;
+      class profanityAPI {
+        getInfo () {
+          return { 
+            id: 'profanityAPI',
+            name: 'profanityAPI',
+            color1: '#cf6a3c',
+            color2: '#cf6a3c',
+            color3: '#cf6a3c',
+            blocks: [
+             {
+              opcode: 'checkProfanity',
+              blockType: Scratch.BlockType.REPORTER,
+              text: "Remove profanity from [TEXT]",
+              arguments: {
+                TEXT: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: 'Hello, I love pizza!',
+                }
+             }
+             },
+          ]
+          }
+        }
+    
+    
+        checkProfanity({TEXT}) {
+            return fetch("https://www.purgomalum.com/service/plain?text="+TEXT)
+            .then(r => r.text())
+            .catch(() => '');
+      }
+    }
+    
+    
+      Scratch.extensions.register(new profanityAPI());
+    })(Scratch);
