658bfa5545168e9620bf8b1f076dc4191d79fc22
DT <66632141+DT-is-not-available@users.noreply.github.com>
2023-03-06 23:36:39 -0500

    oops my bad - deleted unused code

--- extensions/DT/cameracontrols.js
+++ extensions/DT/cameracontrols.js
@@ -9,37 +9,6 @@
 
   const vm = Scratch.vm;
 
-  var vertexShader = ''
-
-  var fragmentShader = ''
-
-  function createShader(gl, type, source) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-      return shader;
-    }
-
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-  }
-
-  function createProgram(gl, vertexShader, fragmentShader) {
-	var program = gl.createProgram();
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-	gl.linkProgram(program);
-	var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-	if (success) {
-	  return program;
-	}
-   
-	console.log(gl.getProgramInfoLog(program));
-	gl.deleteProgram(program);
-  }
-
   let cameraX = 0;
   let cameraY = 0;
   let cameraZoom = 100;
@@ -174,17 +143,6 @@
               }
             }
           },
-          '---',
-          {
-            opcode: 'setCol',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'set background color to [val]',
-            arguments: {
-              val: {
-                type: 'color'
-              }
-            }
-          },
 		  "---",
           {
             opcode: 'getX',
@@ -200,6 +158,17 @@
             opcode: 'getZoom',
             blockType: Scratch.BlockType.REPORTER,
             text: 'camera zoom',
+          },
+          '---',
+          {
+            opcode: 'setCol',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'set background color to [val]',
+            arguments: {
+              val: {
+                type: 'color'
+              }
+            }
           },
         ]
       }
