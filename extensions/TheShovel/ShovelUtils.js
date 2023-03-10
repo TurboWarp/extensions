6b1e370d188af7654254be8797947cb0118b1e4e
GarboMuffin <muffin@mailbox.org>
2023-03-10 12:13:33 -0600

    Rewrite the brightness code in a way that is not a copyright worry

--- extensions/TheShovel/ShovelUtils.js
+++ extensions/TheShovel/ShovelUtils.js
@@ -269,22 +269,9 @@
      * @returns (Number) The brightness value (dark) 0 ... 255 (light)
      */
     brightnessByColor({ color }) {
-      color = "" + color;
-      var isHEX = color.indexOf("#") == 0, isRGB = color.indexOf("rgb") == 0;
-      if (isHEX) {
-        const hasFullSpec = color.length == 7;
-        var m = color.substr(1).match(hasFullSpec ? /(\S{2})/g : /(\S{1})/g);
-        if (m) var r = parseInt(m[0] + (hasFullSpec ? '' : m[0]), 16), g = parseInt(m[1] + (hasFullSpec ? '' : m[1]), 16), b = parseInt(m[2] + (hasFullSpec ? '' : m[2]), 16);
-      }
-      if (isRGB) {
-        m = color.match(/(\d+){3}/g);
-        if (m) r = m[0], g = m[1], b = m[2];
-      }
-      if (Number.isNaN(((r * 299) + (g * 587) + (b * 114)) / 1000)){
-        return "";
-      } else {
-        return ((r * 299) + (g * 587) + (b * 114)) / 1000;
-      }
+      // https://www.w3.org/TR/AERT/#color-contrast
+      const {r, g, b} = Scratch.Cast.toRgbColorObject(color);
+      return ((r * 299) + (g * 587) + (b * 114)) / 1000;
     }
   }
 
