dc8e612f8d8af92d674a6398b8b881c3e9c43a61
GarboMuffin <muffin@mailbox.org>
2023-03-25 21:41:48 -0500

    clean up a lot

--- extensions/TheShovel/CanvasEffects.js
+++ extensions/TheShovel/CanvasEffects.js
@@ -1,30 +1,45 @@
 (function(Scratch) {
     'use strict';
-
-    function setcss() {
-        vm.renderer.canvas.style.filter = 'blur(' + blur + 'px) contrast(' + contrast + ') saturate(' + saturation + '%) hue-rotate(' + color + 'deg) brightness(' + brightness + '%) invert(' + invert + '%)';
-        vm.renderer.canvas.style.imageRendering = rendermode;
-    }
-    var blur = 0;
-    var contrast = 1;
-    var saturation = 100;
-    var color = 0;
-    var brightness = 100;
-    var invert = 0;
-    var rendermode = 'Default';
-    const vm = Scratch.vm;
     if (!Scratch.extensions.unsandboxed) {
         throw new Error('This extension must run unsandboxed');
     }
+
+    const canvas = Scratch.renderer.canvas;
+
+    const updateStyle = () => {
+        const filter = `blur(${blur}px) contrast(${contrast / 100}) saturate(${saturation}%) hue-rotate(${color}deg) brightness(${brightness}%) invert(${invert}%)`;
+        if (canvas.style.filter !== filter) {
+            canvas.style.filter = filter;
+        }
+        const imageRendering = resizeMode === 'pixelated' ? 'pixelated' : '';
+        if (canvas.style.imageRendering !== imageRendering) {
+            canvas.style.imageRendering = imageRendering;
+        }
+    };
+    // scratch-gui will sometimes reset the cursor when resizing the window or going in/out of fullscreen
+    new MutationObserver(updateStyle).observe(canvas, {
+      attributeFilter: ['style'],
+      attributes: true
+    });
+
+    let blur = 0;
+    let contrast = 100;
+    let saturation = 100;
+    let color = 0;
+    let brightness = 100;
+    let invert = 0;
+    let resizeMode = 'default';
+
     class CanvasEffects {
         getInfo() {
             return {
-                id: 'CanvasEffects',
-                name: 'CanvasEffects',
-                blocks: [{
+                id: 'theshovelcanvaseffects',
+                name: 'Canvas Effects',
+                blocks: [
+                    {
                         opcode: 'seteffect',
                         blockType: Scratch.BlockType.COMMAND,
-                        text: 'Set Canvas [EFFECT] to [NUMBER]',
+                        text: 'set canvas [EFFECT] to [NUMBER]',
                         arguments: {
                             EFFECT: {
                                 type: Scratch.ArgumentType.STRING,
@@ -36,30 +51,9 @@
                         },
                     },
                     {
-                        opcode: 'setrendermode',
-                        blockType: Scratch.BlockType.COMMAND,
-                        text: 'Set Canvas render mode [EFFECT]',
-                        arguments: {
-                            EFFECT: {
-                                type: Scratch.ArgumentType.STRING,
-                                menu: 'RENDERMODE'
-                            }
-                        },
-                    },
-                    {
-                        opcode: 'cleareffects',
-                        blockType: Scratch.BlockType.COMMAND,
-                        text: 'Clear Canvas effects'
-                    },
-                    {
-                        opcode: 'refreshcheck',
-                        blockType: Scratch.BlockType.BOOLEAN,
-                        text: 'Is Canvas clear?'
-                    },
-                    {
                         opcode: 'geteffect',
                         blockType: Scratch.BlockType.REPORTER,
-                        text: 'Get Canvas property [EFFECT]',
+                        text: 'get canvas [EFFECT]',
                         arguments: {
                             EFFECT: {
                                 type: Scratch.ArgumentType.STRING,
@@ -68,9 +62,14 @@
                         }
                     },
                     {
+                        opcode: 'cleareffects',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'clear canvas effects'
+                    },
+                    {
                         opcode: 'renderscale',
                         blockType: Scratch.BlockType.COMMAND,
-                        text: 'Set Canvas render size to x:[X] y:[Y]',
+                        text: 'set canvas render size to x:[X] y:[Y]',
                         arguments: {
                             X: {
                                 type: Scratch.ArgumentType.NUMBER,
@@ -82,115 +81,84 @@
                             }
                         }
                     },
+                    {
+                        opcode: 'setrendermode',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'set canvas resize rendering mode [EFFECT]',
+                        arguments: {
+                            EFFECT: {
+                                type: Scratch.ArgumentType.STRING,
+                                menu: 'RENDERMODE'
+                            }
+                        },
+                    },
                 ],
                 menus: {
                     EFFECTMENU: {
                         acceptReporters: true,
-                        items: ['Blur', 'Contrast', 'Saturation', 'Color', 'Brightness', 'Invert']
+                        items: ['blur', 'contrast', 'saturation', 'color shift', 'brightness', 'invert']
                     },
                     RENDERMODE: {
                         acceptReporters: true,
-                        items: ['Pixelated', 'Auto']
+                        items: ['pixelated', 'default']
                     },
                     EFFECTGETMENU: {
                         acceptReporters: true,
-                        items: ['Blur', 'Contrast', 'Saturation', 'Color', 'Brightness', 'Invert', 'Render mode']
+                        items: ['blur', 'contrast', 'saturation', 'color shift', 'brightness', 'invert', 'resize rendering mode']
                     }
                 }
             };
         }
-        geteffect({
-            EFFECT
-        }) {
-            if (EFFECT == 'Blur') {
+        geteffect({EFFECT}) {
+            if (EFFECT === 'blur') {
                 return blur;
-            } else {
-                if (EFFECT == 'Contrast') {
-                    return contrast;
-                } else {
-                    if (EFFECT == 'Saturation') {
-                        return saturation;
-                    } else {
-                        if (EFFECT == 'Color') {
-                            return color;
-                        } else {
-                            if (EFFECT == 'Brightness') {
-                                return brightness;
-                            } else {
-                                if (EFFECT == 'Invert') {
-                                    return invert;
-                                } else {
-                                    if (EFFECT == 'Render mode') {
-                                        return rendermode;
-                                    } else {
-                                        return '';
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+            } else if (EFFECT === 'contrast') {
+                return contrast;
+            } else if (EFFECT === 'saturation') {
+                return saturation;
+            } else if (EFFECT === 'color shift') {
+                return color;
+            } else if (EFFECT === 'brightness') {
+                return brightness;
+            } else if (EFFECT === 'invert') {
+                return invert;
+            } else if (EFFECT === 'resize rendering mode') {
+                return resizeMode;
             }
+            return '';
         }
-        setrendermode({
-            EFFECT
-        }) {
-            rendermode = EFFECT;
-            setcss();
-        }
-        seteffect({
-            EFFECT,
-            NUMBER
-        }) {
-            if (EFFECT == 'Blur') {
+        seteffect({EFFECT, NUMBER}) {
+            if (EFFECT === 'blur') {
                 blur = NUMBER;
-            } else {
-                if (EFFECT == 'Contrast') {
-                    contrast = NUMBER;
-                } else {
-                    if (EFFECT == 'Saturation') {
-                        saturation = NUMBER;
-                    } else {
-                        if (EFFECT == 'Color') {
-                            color = NUMBER;
-                        } else {
-                            if (EFFECT == 'Brightness') {
-                                brightness = NUMBER;
-                            } else {
-                                if (EFFECT == 'Invert') {
-                                    invert = NUMBER;
-                                }
-                            }
-                        }
-                    }
-                }
+            } else if (EFFECT === 'contrast') {
+                contrast = NUMBER;
+            } else if (EFFECT === 'saturation') {
+                saturation = NUMBER;
+            } else if (EFFECT === 'color shift') {
+                color = NUMBER;
+            } else if (EFFECT === 'brightness') {
+                brightness = NUMBER;
+            } else if (EFFECT === 'invert') {
+                invert = NUMBER;
             }
-            setcss();
+            updateStyle();
         }
         cleareffects() {
             blur = 0;
-            contrast = 1;
+            contrast = 100;
             saturation = 100;
             color = 0;
             brightness = 100;
             invert = 0;
-            rendermode = 'Default';
-            vm.renderer.canvas.style.filter = '';
-            vm.renderer.canvas.style.imageRendering = '';
+            resizeMode = 'default';
+            updateStyle();
         }
-        //people can use this to detect if Scratch refreshed the canvas css values, and reapply them
-        refreshcheck() {
-            if (vm.renderer.canvas.style.filter == '' & vm.renderer.canvas.style.imageRendering == '') {
-                return true;
-            } else {
-                return false;
-            }
+        setrendermode({EFFECT}) {
+            resizeMode = EFFECT;
+            updateStyle();
         }
-        renderscale({
-            X,
-            Y
-        }) {
-            vm.renderer.resize(X, Y);
+        renderscale({X, Y}) {
+            Scratch.vm.renderer.resize(X, Y);
         }
     }
     Scratch.extensions.register(new CanvasEffects());
