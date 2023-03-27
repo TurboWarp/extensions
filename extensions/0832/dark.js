7c7730062b43791676e6db4c3a444aafb4b9a70e
0832k12 <70421730+0832k12@users.noreply.github.com>
2023-03-27 22:25:37 +0800

    Add a new extension Dark?

---
+++ extensions/0832/dark.js
@@ -0,0 +1,27 @@
+(function(Scratch) {
+    'use strict';
+  
+    class Dark {
+      getInfo () {
+        return {
+          id: '0832/dark',
+          name: 'Dark?',
+          color1: '#192d50',
+          blocks: [
+            {
+              opcode: 'dark',
+              blockType: Scratch.BlockType.BOOLEAN,
+              text: 'is Darkmode?',
+              arguments: {}
+            }
+          ]
+        };
+      }
+  
+      dark () {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches;
+      }
+    }
+  
+    Scratch.extensions.register(new Dark());
+  })(Scratch);
