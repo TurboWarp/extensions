ecf009e79176cb7223f22520bd65367578dd45ce
NOname-awa <126544888+NOname-awa@users.noreply.github.com>
2023-05-18 17:03:13 +0800

    Update graphics2d.js

--- extensions/NOname-awa/graphics2d.js
+++ extensions/NOname-awa/graphics2d.js
@@ -285,12 +285,12 @@
       return 0;
     }
     graph(args) {
-    try {
-      let points = JSON.parse(args.graph);
-    }
-    catch (error) {
-      return 0;
-    }
+      try {
+        let points = JSON.parse(args.graph);
+      }
+      catch (error) {
+        return 0;
+      }
       let points = JSON.parse (args.graph);
       let n = points.length;
       if (args.CS == 's'){
