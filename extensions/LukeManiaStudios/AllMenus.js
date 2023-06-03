27a74dd93dab5ce2299a4b13a87a8b718cf8ccb1
LukeManiaStudioss <127533508+LukeManiaStudioss@users.noreply.github.com>
2023-06-03 05:52:18 +0100

    Add AllMenus.js

---
+++ extensions/LukeManiaStudios/AllMenus.js
@@ -0,0 +1,78 @@
+(function (Scratch) {
+  'use strict';
+
+  const blacklist = [
+  'looks_costumenumbername'
+  ];
+
+  class allmenus {
+    constructor () {
+      Scratch.vm.runtime.on('EXTENSION_ADDED', () => {
+        refreshMenus()
+      });
+
+    }
+    getInfo() {
+      return {
+        id: 'lmsAllMenus',
+        name: 'All Menus',
+        blocks: [
+          {
+            func: "menureset",
+            blockType: Scratch.BlockType.BUTTON
+          }
+        ]
+      }
+    }
+    menureset() {
+      refreshMenus();
+    }
+  };
+
+  Scratch.vm.addListener('BLOCKSINFO_UPDATE', refreshMenus);
+  refreshMenus();
+
+  function refreshMenus() {
+    if (!window.ScratchBlocks) return;
+
+    Scratch.vm.removeListener('BLOCKSINFO_UPDATE', refreshMenus);
+
+    // Based on code by @Xeltaliv
+    let blockMenu = [];
+    let allBlocks = Object.keys(ScratchBlocks.Blocks);
+    allBlocks = allBlocks.filter(item => item.includes('menu') && !blacklist.includes(item));
+    allBlocks.forEach(item => blockMenu.push('<block id="' + item + '" type="' + item + '"/>'))
+    blockMenu = blockMenu.join('');
+    console.log(blockMenu);
+
+    const menu = function (isInitialSetup, isStage, targetId) {
+      return `
+      <category name="All Menus" id="menublocks" colour="#4C97FF" secondaryColour="#3373CC">
+          ${`<button text="Refresh Blocks" callbackKey="EXTENSION_CALLBACK" callbackData="menublocks_menureset"></button>` + blockMenu}
+          ${categorySeparator}
+      </category>
+      `;
+    }
+
+    const vm = Scratch.vm;
+    const runtime = vm.runtime;
+    const categorySeparator = '<sep gap="36"/>';
+    const gbx = runtime.getBlocksXML.bind(runtime);
+    runtime.getBlocksXML = function(target) {
+      const categoryInfo = this._blockInfo;
+      const res = gbx(target);
+      res.forEach((elem, idx) => {
+        if (categoryInfo[idx].id === "menublocks") {
+          let {editingTarget: target, runtime} = vm;
+          const stage = runtime.getTargetForStage();
+          if (!target) target = stage;
+          elem.xml = menu(false, target.isStage, target.id);
+        }
+      });
+      return res;
+    }
+    Scratch.vm.extensionManager.refreshBlocks();
+  }
+
+Scratch.extensions.register(new allmenus());
+})(Scratch);
