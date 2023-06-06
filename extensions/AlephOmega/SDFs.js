b5932e74cc33352f71bfcbc21b7714028dc1c273
4leph0mega <108088079+4leph0mega@users.noreply.github.com>
2023-06-05 20:52:08 -0600

    Update SDFs.js

--- extensions/AlephOmega/SDFs.js
+++ extensions/AlephOmega/SDFs.js
@@ -1,61 +1,15 @@
-function vecSub(v1, v2) {
-    return { x: v1.x - v2.x, y: v1.y - v2.y, z: v1.z - v2.z };
-  }
-  
-  function vecAdd(v1, v2) {
-    return { x: v1.x + v2.x, y: v1.y + v2.y, z: v1.z + v2.z };
-  }
-  
-  function vecMult(v, s) {
-    return { x: v.x * s, y: v.y * s, z: v.z * s };
-  }
-  
-  function dot(v1, v2) {
-    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-  }
-  
-  function vecDist(v1, v2) {
-    var dx = v1.x - v2.x;
-    var dy = v1.y - v2.y;
-    var dz = v1.z - v2.z;
-    return Math.sqrt(dx * dx + dy * dy + dz * dz);
-  }
-  
-  function max(a,b) {
-   if(a>b) {
-       return a;
-   }
-      else {
-     return b;     
-   }
-  }
-  
-  function min(a,b) {
-   if(a>b) {
-       return b;
-   }
-      else {
-     return a;     
-   }
-  }
-  
-  function clamp(a,b,c) {
-   return max(min(a,b),c)   
-  }
   (function(Scratch) {
-      'use strict';
-    
+      'use strict'
       if (!Scratch.extensions.unsandboxed) {
         throw new Error('This extension must run unsandboxed');
       }
-    
       class SDF {
         getInfo() {
           return {
             color1: "#ad0090",
             color2: "#6e145f",
             color3: "#3b0932",
-            id: 'SDF',
+            id: 'AlephOmegaSDFS"
             name: 'SDFs',
             blocks: [
               {
@@ -442,15 +396,58 @@ function vecSub(v1, v2) {
             ]
           };
         }
+function vecSub(v1, v2) {
+    return { x: v1.x - v2.x, y: v1.y - v2.y, z: v1.z - v2.z };
+  }
   
+  function vecAdd(v1, v2) {
+    return { x: v1.x + v2.x, y: v1.y + v2.y, z: v1.z + v2.z };
+  }
+  
+  function vecMult(v, s) {
+    return { x: v.x * s, y: v.y * s, z: v.z * s };
+  }
+  
+  function dot(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+  }
+  
+  function vecDist(v1, v2) {
+    var dx = v1.x - v2.x;
+    var dy = v1.y - v2.y;
+    var dz = v1.z - v2.z;
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+  }
+  
+  function max(a,b) {
+   if(a > b) {
+       return a;
+   } else {
+       return b;     
+   }
+  }
+  
+  function min(a,b) {
+   if(a > b) {
+       return b;
+   }
+      else {
+     return a;     
+   }
+  }
+  
+  function clamp(a,b,c) {
+   return max(min(a,b),c)   
+  }  
+
         Edge({A, B}) {
           return Math.abs(A + B) - B;
         }
-  
+
         Onioning({A, B}) {
           return Math.abs(A) - B;
         }
-  
+
         Round({A, B}) {
             return A - B;
         }
@@ -458,11 +455,11 @@ function vecSub(v1, v2) {
         Smoothint({A, B, K}) {
           return A * (1 - Math.exp(-B/K));
         }
-  
+
         Smoothsub({A, B, K}) {
           return A * Math.exp(-B/K);
         }
-  
+
         Smoothunion({A, B, K}) {
           return A + B - (A * B)/(A + B) * (1 - Math.exp(-(A + B)/K));
         }
@@ -483,11 +480,11 @@ function vecSub(v1, v2) {
           );
           return dot - B;
         }
-  
+
         round_subtract({A, B, K}) {
           return this.round_intersect({"A": A,"B": 0 - B ,"K": K})
          }
-  
+
         round_intersect({A, B, K}) {
           var ISX = min(A + K,0);
           var ISY = min(B + K,0);
@@ -498,7 +495,7 @@ function vecSub(v1, v2) {
           var indist = min(intersect, -K);
           return indist + outdist;
          }
-  
+
         round_merge({A, B, K}) {
          var ISX = max(A - K,0);
          var ISY = max(B - K,0);
@@ -509,65 +506,61 @@ function vecSub(v1, v2) {
          var outdist = max(union, K);
          return indist + outdist
         }
-  
+
         Groove({A, B, K}) {
           return max(A , 0 - (Math.abs(B) - K))
         }
-  
+
         Hollow({A, B}) {
           return Math.abs(A) - B;
        }
-  
+
         ChampferSub({A, B, K}) {
           var simpleMerge = max(A,0 - B);
           var champfer = (A + (0 - B)) * Math.sqrt(0.5);
           champfer -= K;
           return max(simpleMerge , champfer);
        }
-  
+
         ChampferInt({A, B, K}) {
           var simpleMerge = max(A,B);
           var champfer = (A + B) * Math.sqrt(0.5);
           champfer -= K;
           return max(simpleMerge , champfer);
        }
-  
+
         Champfer({A, B, K}) {
            var simpleMerge = min(A,B);
            var champfer = (A + B) * Math.sqrt(0.5);
            champfer -= K;
            return min(simpleMerge , champfer);
         }
-  
+
         Lerp({A, B, T}) {
           return (A * T) + (B * (1 - T));
         }
-  
+
         Intersection({A, B}) {
           return max(A,B);
         }
-  
+
         Subtraction({A, B}) {
           return min(A,0 - B);
         }
-  
+
         Union({A, B}) {
           return min(A, B);
         }
-  
+
        Capsule({P, A, B, R}) {
     P = JSON.parse(P);
     A = JSON.parse(A);
     B = JSON.parse(B);
-  
     var AP = vecSub(P, A);
     var AB = vecSub(B, A);
-  
     var t = Math.max(0, Math.min(1, dot(AP, AB) / dot(AB, AB)));
-  
     var closestPoint = vecAdd(A, vecMult(AB, t));
     var distToClosest = vecDist(P, closestPoint);
-  
     if (t === 0 || t === 1) {
       return distToClosest - R;
     } else {
@@ -575,7 +568,7 @@ function vecSub(v1, v2) {
       return Math.sqrt(distToClosest * distToClosest + distToAxis * distToAxis);
     }
   }
-  
+
         Sphere({P, A, R}) {
           P = JSON.parse(P);
           A = JSON.parse(A);
@@ -587,39 +580,39 @@ function vecSub(v1, v2) {
           );
           return dist-R;
         }
-  
+
         Box({P, A, S}) {
           P = JSON.parse(P);
           A = JSON.parse(A);
           var dx = P.x - A.x;
           var dy = P.y - A.y;
           var dz = P.z - A.z;
-          dx = Math.abs(dx)-S.x;
-          dy = Math.abs(dy)-S.y;
-          dz = Math.abs(dz)-S.z;
+          dx = Math.abs(dx) - S.x;
+          dy = Math.abs(dy) - S.y;
+          dz = Math.abs(dz) - S.z;
           dx = max(dx,0);
           dy = max(dy,0);
           dz = max(dz,0);
           var dist = Math.sqrt(
-              dx*dx+dy*dy+dz*dz
+              dx * dx + dy * dy + dz * dz
           );
           return dist+min(max(dx,max(dy,dz)),0);
         }
-  
+    
         Box2({P, A, S, R}) {
-                  P = JSON.parse(P);
+          P = JSON.parse(P);
           A = JSON.parse(A);
           var dx = P.x - A.x;
           var dy = P.y - A.y;
           var dz = P.z - A.z;
-          dx = Math.abs(dx)-S.x;
-          dy = Math.abs(dy)-S.y;
-          dz = Math.abs(dz)-S.z;
+          dx = Math.abs(dx) - S.x;
+          dy = Math.abs(dy) - S.y;
+          dz = Math.abs(dz) - S.z;
           dx = max(dx,0)
           dy = max(dy,0)
           dz = max(dz,0)
           var dist = Math.sqrt(
-              dx*dx+dy*dy+dz*dz
+              dx * dx + dy * dy + dz * dz
           );
           return dist+min(max(dx,max(dy,dz)),0)-R
         }
