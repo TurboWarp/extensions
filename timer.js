c02dc573dbb08e74ef908ed34db6fbbf2ab96b5c
MA_master <126303227+MA-master-sec@users.noreply.github.com>
2023-06-11 22:00:37 +0800

    More timer

--- timer.js
+++ timer.js
@@ -1,54 +1,54 @@
-(function(Scratch) {
+(function (Scratch) {
     'use strict';
     const vm = Scratch.vm;
 
     let timer = {};
 
     class Moretimer {
-        
-        getInfo () {
+
+        getInfo() {
             return {
-                color1:"#7f7f7f",
-                id:"timer",
-                name:"more timer",
-                blocks:[
-                {
-                    opcode:"clear_all",
-                    blockType:Scratch.BlockType.COMMAND,
-                    text:"clear all timer"
-                },
-                {
-                    opcode:"clear",
-                    blockType:Scratch.BlockType.COMMAND,
-                    text:"start or reset timer[T]",
-                    arguments:{
-                        T:{
-                            type:Scratch.ArgumentType.STRING,
-                            defaultValue: "mytimer",
+                color1: "#7f7f7f",
+                id: "timer",
+                name: "more timer",
+                blocks: [
+                    {
+                        opcode: "clear_all",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "clear all timer"
+                    },
+                    {
+                        opcode: "clear",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "start or reset timer[T]",
+                        arguments: {
+                            T: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "mytimer",
+                            }
+                        }
+                    },
+                    {
+                        opcode: "get",
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "timer[T]",
+                        arguments: {
+                            T: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "mytimer",
+                            }
                         }
                     }
-                },
-                {
-                    opcode:"get",
-                    blockType:Scratch.BlockType.REPORTER,
-                    text:"timer[T]",
-                    arguments:{
-                        T:{
-                            type:Scratch.ArgumentType.STRING,
-                            defaultValue:"mytimer",
-                        }
-                    }
-                }
                 ]
             };
-        }  
-        clear_all(args, util){
+        }
+        clear_all(args, util) {
             timer = {};
         }
-        clear(args, util){
+        clear(args, util) {
             timer[args.T.toString()] = Date.now();
         }
-        get(args, util){
+        get(args, util) {
             return (Date.now() - timer[args.T.toString()]) / 1000;
         }
     }
