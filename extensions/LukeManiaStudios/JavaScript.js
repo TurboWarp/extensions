4e4f57273df2ca2ba5b27a8a4fbc7aae61592205
LukeManiaStudioss <127533508+LukeManiaStudioss@users.noreply.github.com>
2023-06-12 23:24:38 +0100

    shut up, eslint (round 1)

--- extensions/LukeManiaStudios/JavaScript.js
+++ extensions/LukeManiaStudios/JavaScript.js
@@ -14,24 +14,23 @@
 
   // Credit to TheShovel for the GUI element.
   const para = document.createElement("p");
-  if(ineditor == false) {
+  if (ineditor == false) {
     try {
-      scaffolding
-    }
-    catch(err) {
+      scaffolding;
+    } catch (error) {
       para.innerText = "JavaScript: JavaScript is currently disabled.";
       document.querySelector("#app > div > div > div > div.gui_body-wrapper_-N0sA.box_box_2jjDp > div > div.gui_stage-and-target-wrapper_69KBf.box_box_2jjDp > div.stage-wrapper_stage-wrapper_2bejr.box_box_2jjDp > div.stage-wrapper_stage-canvas-wrapper_3ewmd.box_box_2jjDp").appendChild(para);
     }
   }
   try {
-    scaffolding
+    scaffolding;
     setTimeout(function(){
     checkifexists();
     }, 100);
-  } catch(err) {
+  } catch (error) {
     ineditor = true;
   }
-    
+
   class CustomJS {
     getInfo() {
       return {
@@ -56,7 +55,7 @@
             blockType: Scratch.BlockType.BOOLEAN,
             text: 'is JS allowed?'
           },
-          
+
           '---',
 
           {
@@ -104,7 +103,7 @@
         'Enabling this will allow the project to run dangerous scripts.' +
         '\n' +
         'Do not enable this unless you know exactly what this does.'
-      )
+      );
       if (confirm(alert)) {
         allowJSCode = true;
         para.style.display = 'none';
