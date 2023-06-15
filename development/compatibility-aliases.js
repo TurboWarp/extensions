0fccc6d43e674a6f095297969e1678d412092cd4
GarboMuffin <muffin@mailbox.org>
2023-06-15 18:28:42 -0500

    Compatibility

---
+++ development/compatibility-aliases.js
@@ -0,0 +1,13 @@
+const extensions = {
+  // maps old path to new path
+  '/LukeManiaStudios/ClonesPlus.js': '/LilyMakesThings/ClonesPlus.js',
+  '/LukeManiaStudios/CommentBlocks.js': '/LilyMakesThings/CommentBlocks.js',
+  '/LukeManiaStudios/lmsutils.js': '/LilyMakesThings/lmsutils.js',
+  '/LukeManiaStudios/LooksPlus.js': '/LilyMakesThings/LooksPlus.js',
+  '/LukeManiaStudios/McUtils.js': '/LilyMakesThings/McUtils.js',
+  '/LukeManiaStudios/MoreTimers.js': '/LilyMakesThings/MoreTimers.js',
+  '/LukeManiaStudios/TempVariables.js': '/LilyMakesThings/TempVariables.js',
+  '/LukeManiaStudios/TempVariables2.js': '/LilyMakesThings/TempVariables2.js'
+};
+
+module.exports = extensions;
