2c6798194c48910a1f21c0fa1452e93c95c86c3d
GarboMuffin <muffin@mailbox.org>
2023-06-16 21:57:40 -0500

    Rename again

--- development/compatibility-aliases.js
+++ development/compatibility-aliases.js
@@ -1,13 +1,13 @@
 const extensions = {
   // maps old path to new path
-  '/LukeManiaStudios/ClonesPlus.js': '/LilyMakesThings/ClonesPlus.js',
-  '/LukeManiaStudios/CommentBlocks.js': '/LilyMakesThings/CommentBlocks.js',
-  '/LukeManiaStudios/lmsutils.js': '/LilyMakesThings/lmsutils.js',
-  '/LukeManiaStudios/LooksPlus.js': '/LilyMakesThings/LooksPlus.js',
-  '/LukeManiaStudios/McUtils.js': '/LilyMakesThings/McUtils.js',
-  '/LukeManiaStudios/MoreTimers.js': '/LilyMakesThings/MoreTimers.js',
-  '/LukeManiaStudios/TempVariables.js': '/LilyMakesThings/TempVariables.js',
-  '/LukeManiaStudios/TempVariables2.js': '/LilyMakesThings/TempVariables2.js'
+  '/LukeManiaStudios/ClonesPlus.js': '/Lily/ClonesPlus.js',
+  '/LukeManiaStudios/CommentBlocks.js': '/Lily/CommentBlocks.js',
+  '/LukeManiaStudios/lmsutils.js': '/Lily/lmsutils.js',
+  '/LukeManiaStudios/LooksPlus.js': '/Lily/LooksPlus.js',
+  '/LukeManiaStudios/McUtils.js': '/Lily/McUtils.js',
+  '/LukeManiaStudios/MoreTimers.js': '/Lily/MoreTimers.js',
+  '/LukeManiaStudios/TempVariables.js': '/Lily/TempVariables.js',
+  '/LukeManiaStudios/TempVariables2.js': '/Lily/TempVariables2.js'
 };
 
 module.exports = extensions;
