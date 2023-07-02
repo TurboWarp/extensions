d770bd11366e6506820d94a64b5c839669b773dd
Twingamerdudes <81382687+Twingamerdudes@users.noreply.github.com>
2023-07-02 18:30:12 -0400

    Error handling on some blocks

--- extensions/Twingamerdudes/modals.js
+++ extensions/Twingamerdudes/modals.js
@@ -135,9 +135,8 @@
     }
 
     addTextToModal(args, util){
-      if(isModalOpen){
+      if (isModalOpen){
         const text = args.TEXT;
-
         //select the first <dialog> element
         const modal = document.querySelector("dialog");
         const textNode = document.createTextNode(text);
@@ -148,22 +147,28 @@
     }
 
     changeDefaultModalText(args, util){
-      const defaultModalText = args.TEXT;
-      const modal = document.querySelector("dialog");
-      const text = document.getElementById("randomutilities_modalText");
-      text.textContent = defaultModalText;
+      if (isModalOpen){
+        const defaultModalText = args.TEXT;
+        const modal = document.querySelector("dialog");
+        const text = document.getElementById("randomutilities_modalText");
+        text.textContent = defaultModalText;
+      }
     }
 
     changeDefaultModalColor(args, util){
-      const defaultModalColor = args.COLOR;
-      const modal = document.querySelector("dialog");
-      modal.style.backgroundColor = defaultModalColor;
+      if (isModalOpen){
+        const defaultModalColor = args.COLOR;
+        const modal = document.querySelector("dialog");
+        modal.style.backgroundColor = defaultModalColor;
+      }
     }
 
     changeDefaultModalTextColor(args, util){
-      const defaultModalTextColor = args.COLOR;
-      const modal = document.querySelector("dialog");
-      modal.style.color = defaultModalTextColor;
+      if (isModalOpen){
+        const defaultModalTextColor = args.COLOR;
+        const modal = document.querySelector("dialog");
+        modal.style.color = defaultModalTextColor;
+      }
     }
 
     IsModalOpen(){
@@ -175,7 +180,7 @@
     }
     showModal(args, util){
       //Create Modal
-      if(!isModalOpen){
+      if (!isModalOpen){
         var modal = document.createElement("dialog");
         var text = document.createElement("p");
         text.id = "randomutilities_modalText";
@@ -218,104 +223,103 @@
     }
 
     showModalWithInput(args, util){
+      if (!isModalOpen){
+        //Create Modal
+        var modal = document.createElement("dialog");
+        var text = document.createElement("p");
+        text.id = "randomutilities_modalText";
+        text.textContent = args.TEXT;
+        modal.appendChild(text);
+        document.body.appendChild(modal);
 
-      if(!isModalOpen){
-      //Create Modal
-      var modal = document.createElement("dialog");
-      var text = document.createElement("p");
-      text.id = "randomutilities_modalText";
-      text.textContent = args.TEXT;
-      modal.appendChild(text);
-      document.body.appendChild(modal);
+        //Create input
+        var input = document.createElement("input");
+        input.placeholder = args.PLACEHOLDER;
 
-      //Create input
-      var input = document.createElement("input");
-      input.placeholder = args.PLACEHOLDER;
+        //CSS Hell
+        input.type = "text";
+        input.id = "randomutilities_modalInput";
+        input.style.width = "100%";
+        input.style.margin = "10px 0";
+        input.style.padding = "5px";
+        input.style.border = "none";
+        input.style.background = args.SCOLOR;
+        input.style.fontSize = "0.8em";
+        input.style.outline = "none";
+        input.style.transformOrigin = "50% 50%";
+        input.style.zIndex = "100";
+        input.style.borderRadius = "5px";
+        input.style.color = args.TCOLOR;
 
-      //CSS Hell
-      input.type = "text";
-      input.id = "randomutilities_modalInput";
-      input.style.width = "100%";
-      input.style.margin = "10px 0";
-      input.style.padding = "5px";
-      input.style.border = "none";
-      input.style.background = args.SCOLOR;
-      input.style.fontSize = "0.8em";
-      input.style.outline = "none";
-      input.style.transformOrigin = "50% 50%";
-      input.style.zIndex = "100";
-      input.style.borderRadius = "5px";
-      input.style.color = args.TCOLOR;
+        //WHY DO I HAVE TO DO THIS!?!?!?!?!?!?
+        var pcss = "#randomutilities_modalInput::placeholder { color: " + args.TCOLOR + "; opacity: 0.5; }";
+        var styleElement = document.createElement("style");
+        styleElement.appendChild(document.createTextNode(pcss));
+        document.head.appendChild(styleElement);
 
-      //WHY DO I HAVE TO DO THIS!?!?!?!?!?!?
-      var pcss = "#randomutilities_modalInput::placeholder { color: " + args.TCOLOR + "; opacity: 0.5; }";
-      var styleElement = document.createElement("style");
-      styleElement.appendChild(document.createTextNode(pcss));
-      document.head.appendChild(styleElement);
+        //Create Close Button
+        var close = document.createElement("button");
+        close.innerHTML = "X";
+        close.addEventListener("click", function(){
+            util.startHats('randomutilities_modalClose');
+            isModalOpen = false;
+            modal.close();
+            modal.remove();
+        });
 
-      //Create Close Button
-      var close = document.createElement("button");
-      close.innerHTML = "X";
-      close.addEventListener("click", function(){
+        //More CSS Hell
+        close.style.position = "absolute";
+        close.style.top = "0";
+        close.style.right = "0";
+        close.style.padding = "5px";
+        close.style.border = "none";
+        close.style.background = "#ff0000";
+        close.style.fontSize = "0.8em";
+        close.style.cursor = "pointer";
+        close.style.outline = "none";
+        close.style.transformOrigin = "50% 50%";
+        close.style.zIndex = "0";
+        close.style.borderRadius = "0 0 0 5px";
+
+        //Create Submit Button
+        var submit = document.createElement("button");
+        submit.innerHTML = "Submit";
+        submit.addEventListener("click", function(){
           util.startHats('randomutilities_modalClose');
+          const input = document.getElementById("randomutilities_modalInput");
           isModalOpen = false;
+          // @ts-ignore
+          modalInput = input.value;
           modal.close();
           modal.remove();
-      });
+        });
 
-      //More CSS Hell
-      close.style.position = "absolute";
-      close.style.top = "0";
-      close.style.right = "0";
-      close.style.padding = "5px";
-      close.style.border = "none";
-      close.style.background = "#ff0000";
-      close.style.fontSize = "0.8em";
-      close.style.cursor = "pointer";
-      close.style.outline = "none";
-      close.style.transformOrigin = "50% 50%";
-      close.style.zIndex = "0";
-      close.style.borderRadius = "0 0 0 5px";
+        //More CSS Hell
+        submit.style.backgroundColor = args.SCOLOR;
+        submit.style.color = args.TCOLOR;
+        submit.style.border = "none";
+        submit.style.padding = "5px";
+        submit.style.fontSize = "1em";
+        submit.style.cursor = "pointer";
+        submit.style.outline = "none";
+        submit.style.transformOrigin = "50% 50%";
+        submit.style.zIndex = "0";
+        submit.style.borderRadius = "5px";
 
-      //Create Submit Button
-      var submit = document.createElement("button");
-      submit.innerHTML = "Submit";
-      submit.addEventListener("click", function(){
-        util.startHats('randomutilities_modalClose');
-        const input = document.getElementById("randomutilities_modalInput");
-        isModalOpen = false;
-        // @ts-ignore
-        modalInput = input.value;
-        modal.close();
-        modal.remove();
-      });
-
-      //More CSS Hell
-      submit.style.backgroundColor = args.SCOLOR;
-      submit.style.color = args.TCOLOR;
-      submit.style.border = "none";
-      submit.style.padding = "5px";
-      submit.style.fontSize = "1em";
-      submit.style.cursor = "pointer";
-      submit.style.outline = "none";
-      submit.style.transformOrigin = "50% 50%";
-      submit.style.zIndex = "0";
-      submit.style.borderRadius = "5px";
-
-      //Finish Modal
-      modal.style.width = "300px";
-      modal.style.padding = "20px 30px";
-      modal.style.textAlign = "center";
-      modal.style.background = args.COLOR;
-      modal.style.color = args.TCOLOR;
-      modal.style.overflow = "hidden";
-      modal.appendChild(close);
-      modal.appendChild(input);
-      modal.appendChild(submit);
-      isModalOpen = true;
-      modal.showModal();
-      util.startHats('randomutilities_modalOpen');
-    }
+        //Finish Modal
+        modal.style.width = "300px";
+        modal.style.padding = "20px 30px";
+        modal.style.textAlign = "center";
+        modal.style.background = args.COLOR;
+        modal.style.color = args.TCOLOR;
+        modal.style.overflow = "hidden";
+        modal.appendChild(close);
+        modal.appendChild(input);
+        modal.appendChild(submit);
+        isModalOpen = true;
+        modal.showModal();
+        util.startHats('randomutilities_modalOpen');
+      }
   }
 }
 Scratch.extensions.register(new RandomUtils());
