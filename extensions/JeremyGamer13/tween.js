7044449308f29f141cd64b818d5e1faff9609b09
GarboMuffin <muffin@mailbox.org>
2023-07-16 11:21:31 -0500

    format code

--- extensions/JeremyGamer13/tween.js
+++ extensions/JeremyGamer13/tween.js
@@ -19,11 +19,8 @@
     const ArgumentType = Scratch.ArgumentType;
     const Cast = Scratch.Cast;
     const vm = Scratch.vm;
-    const runtime = vm.runtime;
 
     class Tween {
-        clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-
         getInfo() {
             return {
                 id: 'jeremygamerTweening',
@@ -89,7 +86,7 @@
                 menus: {
                     modes: {
                         acceptReporters: true,
-                        items: EasingMethods.map(item => ({ text: item, value: item }))
+                        items: EasingMethods
                     },
                     direction: {
                         acceptReporters: true,
@@ -97,7 +94,7 @@
                             "in",
                             "out",
                             "in out"
-                        ].map(item => ({ text: item, value: item }))
+                        ]
                     }
                 }
             };
@@ -108,6 +105,9 @@
             const multiplier = end - start;
             const result = (mul * multiplier) + start;
             return result;
+        }
+        clamp(num, min, max) {
+            return Math.min(Math.max(num, min), max);
         }
 
         // blocks
@@ -197,226 +197,224 @@
                 util.yield();
             }
         }
-        
-    
 
-    // easing functions (placed below blocks for organization)
-    linear(x) {
-        // lol
-        return x;
-    }
-
-    sine(x, dir) {
-        switch (dir) {
-            case "in": {
-                return 1 - Math.cos((x * Math.PI) / 2);
-            }
-            case "out": {
-                return Math.sin((x * Math.PI) / 2);
-            }
-            case "in out": {
-                return -(Math.cos(Math.PI * x) - 1) / 2;
-            }
-            default:
-                return 0;
+        // easing functions (placed below blocks for organization)
+        linear(x) {
+            // lol
+            return x;
         }
-    }
 
-    quad(x, dir) {
-        switch (dir) {
-            case "in": {
-                return x * x;
-            }
-            case "out": {
-                return 1 - (1 - x) * (1 - x);
-            }
-            case "in out": {
-                return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    cubic(x, dir) {
-        switch (dir) {
-            case "in": {
-                return x * x * x;
-            }
-            case "out": {
-                return 1 - Math.pow(1 - x, 3);
-            }
-            case "in out": {
-                return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    quart(x, dir) {
-        switch (dir) {
-            case "in": {
-                return x * x * x * x;
-            }
-            case "out": {
-                return 1 - Math.pow(1 - x, 4);
-            }
-            case "in out": {
-                return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    quint(x, dir) {
-        switch (dir) {
-            case "in": {
-                return x * x * x * x * x;
-            }
-            case "out": {
-                return 1 - Math.pow(1 - x, 5);
-            }
-            case "in out": {
-                return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    expo(x, dir) {
-        switch (dir) {
-            case "in": {
-                return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
-            }
-            case "out": {
-                return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
-            }
-            case "in out": {
-                return x === 0
-                    ? 0
-                    : x === 1
-                        ? 1
-                        : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
-                            : (2 - Math.pow(2, -20 * x + 10)) / 2;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    circ(x, dir) {
-        switch (dir) {
-            case "in": {
-                return 1 - Math.sqrt(1 - Math.pow(x, 2));
-            }
-            case "out": {
-                return Math.sqrt(1 - Math.pow(x - 1, 2));
-            }
-            case "in out": {
-                return x < 0.5
-                    ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
-                    : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    back(x, dir) {
-        switch (dir) {
-            case "in": {
-                const c1 = 1.70158;
-                const c3 = c1 + 1;
-
-                return c3 * x * x * x - c1 * x * x;
-            }
-            case "out": {
-                const c1 = 1.70158;
-                const c3 = c1 + 1;
-
-                return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
-            }
-            case "in out": {
-                const c1 = 1.70158;
-                const c2 = c1 * 1.525;
-
-                return x < 0.5
-                    ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-                    : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    elastic(x, dir) {
-        switch (dir) {
-            case "in": {
-                const c4 = (2 * Math.PI) / 3;
-
-                return x === 0
-                    ? 0
-                    : x === 1
-                        ? 1
-                        : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
-            }
-            case "out": {
-                const c4 = (2 * Math.PI) / 3;
-
-                return x === 0
-                    ? 0
-                    : x === 1
-                        ? 1
-                        : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
-            }
-            case "in out": {
-                const c5 = (2 * Math.PI) / 4.5;
-
-                return x === 0
-                    ? 0
-                    : x === 1
-                        ? 1
-                        : x < 0.5
-                            ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
-                            : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
-            }
-            default:
-                return 0;
-        }
-    }
-
-    bounce(x, dir) {
-        switch (dir) {
-            case "in": {
-                return 1 - this.bounce(1 - x, "out");
-            }
-            case "out": {
-                const n1 = 7.5625;
-                const d1 = 2.75;
-
-                if (x < 1 / d1) {
-                    return n1 * x * x;
-                } else if (x < 2 / d1) {
-                    return n1 * (x -= 1.5 / d1) * x + 0.75;
-                } else if (x < 2.5 / d1) {
-                    return n1 * (x -= 2.25 / d1) * x + 0.9375;
-                } else {
-                    return n1 * (x -= 2.625 / d1) * x + 0.984375;
+        sine(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return 1 - Math.cos((x * Math.PI) / 2);
                 }
+                case "out": {
+                    return Math.sin((x * Math.PI) / 2);
+                }
+                case "in out": {
+                    return -(Math.cos(Math.PI * x) - 1) / 2;
+                }
+                default:
+                    return 0;
             }
-            case "in out": {
-                return x < 0.5
-                    ? (1 - this.bounce(1 - 2 * x, "out")) / 2
-                    : (1 + this.bounce(2 * x - 1, "out")) / 2;
+        }
+
+        quad(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return x * x;
+                }
+                case "out": {
+                    return 1 - (1 - x) * (1 - x);
+                }
+                case "in out": {
+                    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+                }
+                default:
+                    return 0;
             }
-            default:
-                return 0;
+        }
+
+        cubic(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return x * x * x;
+                }
+                case "out": {
+                    return 1 - Math.pow(1 - x, 3);
+                }
+                case "in out": {
+                    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+        quart(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return x * x * x * x;
+                }
+                case "out": {
+                    return 1 - Math.pow(1 - x, 4);
+                }
+                case "in out": {
+                    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+        quint(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return x * x * x * x * x;
+                }
+                case "out": {
+                    return 1 - Math.pow(1 - x, 5);
+                }
+                case "in out": {
+                    return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+        expo(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+                }
+                case "out": {
+                    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+                }
+                case "in out": {
+                    return x === 0
+                        ? 0
+                        : x === 1
+                            ? 1
+                            : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+                                : (2 - Math.pow(2, -20 * x + 10)) / 2;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+        circ(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return 1 - Math.sqrt(1 - Math.pow(x, 2));
+                }
+                case "out": {
+                    return Math.sqrt(1 - Math.pow(x - 1, 2));
+                }
+                case "in out": {
+                    return x < 0.5
+                        ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
+                        : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+        back(x, dir) {
+            switch (dir) {
+                case "in": {
+                    const c1 = 1.70158;
+                    const c3 = c1 + 1;
+
+                    return c3 * x * x * x - c1 * x * x;
+                }
+                case "out": {
+                    const c1 = 1.70158;
+                    const c3 = c1 + 1;
+
+                    return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+                }
+                case "in out": {
+                    const c1 = 1.70158;
+                    const c2 = c1 * 1.525;
+
+                    return x < 0.5
+                        ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+                        : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+        elastic(x, dir) {
+            switch (dir) {
+                case "in": {
+                    const c4 = (2 * Math.PI) / 3;
+
+                    return x === 0
+                        ? 0
+                        : x === 1
+                            ? 1
+                            : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
+                }
+                case "out": {
+                    const c4 = (2 * Math.PI) / 3;
+
+                    return x === 0
+                        ? 0
+                        : x === 1
+                            ? 1
+                            : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+                }
+                case "in out": {
+                    const c5 = (2 * Math.PI) / 4.5;
+
+                    return x === 0
+                        ? 0
+                        : x === 1
+                            ? 1
+                            : x < 0.5
+                                ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+                                : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+        bounce(x, dir) {
+            switch (dir) {
+                case "in": {
+                    return 1 - this.bounce(1 - x, "out");
+                }
+                case "out": {
+                    const n1 = 7.5625;
+                    const d1 = 2.75;
+
+                    if (x < 1 / d1) {
+                        return n1 * x * x;
+                    } else if (x < 2 / d1) {
+                        return n1 * (x -= 1.5 / d1) * x + 0.75;
+                    } else if (x < 2.5 / d1) {
+                        return n1 * (x -= 2.25 / d1) * x + 0.9375;
+                    } else {
+                        return n1 * (x -= 2.625 / d1) * x + 0.984375;
+                    }
+                }
+                case "in out": {
+                    return x < 0.5
+                        ? (1 - this.bounce(1 - 2 * x, "out")) / 2
+                        : (1 + this.bounce(2 * x - 1, "out")) / 2;
+                }
+                default:
+                    return 0;
+            }
         }
     }
-}
 
     Scratch.extensions.register(new Tween());
 }) (Scratch);
