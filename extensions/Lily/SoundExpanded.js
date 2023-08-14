85f1222f18bbc7f82bdfc9f110005cd901e4311b
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2023-08-14 05:09:11 +0100

    Update SoundExpanded.js

--- extensions/Lily/SoundExpanded.js
+++ extensions/Lily/SoundExpanded.js
@@ -191,7 +191,7 @@
 
       if (!soundPlayer.isPlaying) {
         soundCategory._addWaitingSound(target.id, soundId);
-        sprite.soundBank.playSound(util.target, soundId);  
+        sprite.soundBank.playSound(util.target, soundId);
       }
 
       if (!soundPlayer.outputNode) return;
