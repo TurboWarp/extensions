c910a9b89bef62d47f50cc265f835273c071b8e2
Muffin <muffin@mailbox.org>
2023-08-23 10:37:33 -0500

    Remove unused functions

--- extensions/Lily/Data.js
+++ extensions/Lily/Data.js
@@ -23,20 +23,6 @@
     if (listObject) return listObject;
   };
 
-  const cloneObj = function (original) {
-    return JSON.parse(JSON.stringify(original));
-  };
-
-  const arraysEqual = function (a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length !== b.length) return false;
-    for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  };
-
   class Data {
     getInfo() {
       return {
