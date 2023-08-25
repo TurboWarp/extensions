b17c2b36c183bb883fd49c83f4168c9b7ed9b661
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2023-08-25 18:14:30 +0100

    Update SoundExpanded.js

--- extensions/Lily/SoundExpanded.js
+++ extensions/Lily/SoundExpanded.js
@@ -293,7 +293,7 @@
     changeProjectVolume(args) {
       const value = Scratch.Cast.toNumber(args.VALUE) / 100;
       const volume = runtime.audioEngine.inputNode.gain.value;
-      const newVolume = this._wrapClamp(volume + value, 0, 1);
+      const newVolume = Scratch.Cast.toNumber(Math.min(Math.max(volume + value, 1), 0));
       runtime.audioEngine.inputNode.gain.value = newVolume;
     }
 
