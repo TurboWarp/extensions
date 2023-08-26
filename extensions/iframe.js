ed74f5893be22e9d47940a00401dc1fcc0c903a8
GarboMuffin <muffin@mailbox.org>
2023-08-26 17:57:51 -0500

    iframe: various fixes (#955)
    - update default page
    - add resize behavior option
    - fix position after custom stage size changes

--- extensions/iframe.js
+++ extensions/iframe.js
@@ -7,6 +7,7 @@
 
   /** @type {HTMLIFrameElement|null} */
   let iframe = null;
+  let overlay = null;
 
   const featurePolicy = {
     accelerometer: "'none'",
@@ -39,32 +40,48 @@
   let width = -1; // negative means default
   let height = -1; // negative means default
   let interactive = true;
+  let resizeBehavior = "scale";
 
   const updateFrameAttributes = () => {
     if (!iframe) {
       return;
     }
 
+    iframe.style.pointerEvents = interactive ? "auto" : "none";
+
     const { stageWidth, stageHeight } = Scratch.vm.runtime;
     const effectiveWidth = width >= 0 ? width : stageWidth;
     const effectiveHeight = height >= 0 ? height : stageHeight;
-    iframe.style.width = `${effectiveWidth}px`;
-    iframe.style.height = `${effectiveHeight}px`;
 
-    let transform = "";
-    transform += `translate(${stageWidth / 2 - effectiveWidth / 2 + x}px,${
-      stageHeight / 2 - effectiveHeight / 2 - y
-    }px)`;
-    iframe.style.transform = transform;
+    if (resizeBehavior === "scale") {
+      iframe.style.width = `${effectiveWidth}px`;
+      iframe.style.height = `${effectiveHeight}px`;
 
-    iframe.style.pointerEvents = interactive ? "auto" : "none";
+      iframe.style.transform = `translate(${-effectiveWidth / 2 + x}px, ${
+        -effectiveHeight / 2 - y
+      }px)`;
+      iframe.style.top = "0";
+      iframe.style.left = "0";
+    } else {
+      // As the stage is resized in fullscreen mode, only % can be relied upon
+      iframe.style.width = `${(effectiveWidth / stageWidth) * 100}%`;
+      iframe.style.height = `${(effectiveHeight / stageHeight) * 100}%`;
+
+      iframe.style.transform = "";
+      iframe.style.top = `${(0.5 - effectiveHeight / 2 / stageHeight) * 100}%`;
+      iframe.style.left = `${(0.5 - effectiveWidth / 2 / stageWidth) * 100}%`;
+    }
   };
+
+  const getOverlayMode = () =>
+    resizeBehavior === "scale" ? "scale-centered" : "manual";
 
   const createFrame = (src) => {
     iframe = document.createElement("iframe");
     iframe.style.width = "100%";
     iframe.style.height = "100%";
     iframe.style.border = "none";
+    iframe.style.position = "absolute";
     iframe.setAttribute("sandbox", "allow-scripts allow-same-origin");
     iframe.setAttribute(
       "allow",
@@ -75,10 +92,12 @@
     iframe.setAttribute("allowtransparency", "true");
     iframe.setAttribute("allowtransparency", "true");
     iframe.setAttribute("src", src);
-    Scratch.renderer.addOverlay(iframe, "scale");
 
+    overlay = Scratch.renderer.addOverlay(iframe, getOverlayMode());
     updateFrameAttributes();
   };
+
+  Scratch.vm.on("STAGE_SIZE_CHANGED", updateFrameAttributes);
 
   class IframeExtension {
     getInfo() {
@@ -191,6 +210,17 @@
               },
             },
           },
+          {
+            opcode: "setResize",
+            blockType: Scratch.BlockType.COMMAND,
+            text: "set iframe resize behavior to [RESIZE]",
+            arguments: {
+              RESIZE: {
+                type: Scratch.ArgumentType.STRING,
+                menu: "resizeMenu",
+              },
+            },
+          },
         ],
         menus: {
           getMenu: {
@@ -203,11 +233,16 @@
               "width",
               "height",
               "interactive",
+              "resize behavior",
             ],
           },
           interactiveMenu: {
             acceptReporters: true,
             items: ["true", "false"],
+          },
+          resizeMenu: {
+            acceptReporters: true,
+            items: ["scale", "viewport"],
           },
         },
       };
@@ -246,6 +281,7 @@
       if (iframe) {
         Scratch.renderer.removeOverlay(iframe);
         iframe = null;
+        overlay = null;
       }
     }
 
@@ -266,6 +302,8 @@
         return height >= 0 ? height : Scratch.vm.runtime.stageHeight;
       } else if (MENU === "interactive") {
         return interactive;
+      } else if (MENU === "resize behavior") {
+        return resizeBehavior;
       } else {
         return "";
       }
@@ -294,6 +332,17 @@
     setInteractive({ INTERACTIVE }) {
       interactive = Scratch.Cast.toBoolean(INTERACTIVE);
       updateFrameAttributes();
+    }
+
+    setResize({ RESIZE }) {
+      if (RESIZE === "scale" || RESIZE === "viewport") {
+        resizeBehavior = RESIZE;
+        if (overlay) {
+          overlay.mode = getOverlayMode();
+          Scratch.renderer._updateOverlays();
+          updateFrameAttributes();
+        }
+      }
     }
   }
 
