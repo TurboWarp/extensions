280095b625d9c24a91931f60dd439abe43c0af06
happen3 <110855596+happen3@users.noreply.github.com>
2023-08-28 20:20:43 +0200

    Create additionalroots.js

---
+++ happen3/additionalroots.js
@@ -0,0 +1,37 @@
+(function (ext) {
+    ext.squareRoot = function (num) {
+        return Math.sqrt(num);
+    };
+
+    ext.cubeRoot = function (num) {
+        return Math.cbrt(num);
+    };
+
+    ext.fourthRoot = function (num) {
+        return Math.pow(num, 1 / 4);
+    };
+
+    ext.fifthRoot = function (num) {
+        return Math.pow(num, 1 / 5);
+    };
+
+    ext.nthRoot = function (n, nth) {
+        return Math.pow(n, 1 / nth);
+    };
+
+    // extension data
+    var descriptor = {
+        blocks: [
+            ['r', 'square root of %n', 'squareRoot', 9],
+            ['r', 'cube root of %n', 'cubeRoot', 8],
+            ['r', 'fourth root of %n', 'fourthRoot', 16],
+            ['r', 'fifth root of %n', 'fifthRoot', 32],
+            ['r', '%n to the power of 1/%n', 'nthRoot', 4, 2],
+        ],
+        menus: {},
+        displayName: 'Additional Roots!',
+    };
+
+    // register the extension
+    ScratchExtensions.register('additionalRoots', descriptor, ext);
+})({});
