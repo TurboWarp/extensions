46a6cdcab30e47cabc7100f30e959a5304cbe42d
Themadpunter <106832853+familycomicsstudios@users.noreply.github.com>
2023-09-03 11:39:18 -0500

    Update geolocation.js

--- extensions/Themadpunter/geolocation.js
+++ extensions/Themadpunter/geolocation.js
@@ -2,11 +2,16 @@ class GeolocationExtension {
   constructor(runtime) {
     this.runtime = runtime;
   }
+  
 
   getInfo() {
+    const blockIconURI = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAAzCAYAAAAKLSELAAAAAXNSR0IArs4c6QAAARNJREFUaEPt2OEKwyAMRtHl/R/aYkHpnDYm+WqT4f4O5HBt0YY+oF9KKbVLEREhllcv0kNxIC1ahNTARnAJeAqJxGkeCRb5JLCAuaq3yBXAGegQuRLIQbvIN4B30B/km8AR9AvpAdiDxkJ6qtjWrCXdIz0CrzXPkhvJXY2Y//ORSZ4r1i3fSONW75KggHWZ/eKgisYouU8cwH6fJ06Ykl6hJWKc+2R5dDyd49eBQaxvHE8127FLvO9uDzV7w6u4s6C3io5GgLFHfytrmoaoK6AcMBvYcfST0Bng/yHRN6XZiqKSyG2XAFVIa1EpUI3UQjVAE1IK1QLNyFmoBQhBclArEIYcQRFAKLKFooBwZIEigXnNAzqQzBTaigTKAAAAAElFTkSuQmCC";
     return {
       id: 'geolocation',
       name: 'Geolocation',
+      color1: '#12e8f7',
+      color2: '#11aacc',
+      blockIconURI: blockIconURI,
       blocks: [
         {
           opcode: 'getLatitude',
