d771ccb36771c7c1070aad695501e3bf85e52bb1
NexusKitten <127152751+NexusKitten@users.noreply.github.com>
2023-09-05 12:46:57 -0400

    Format fixes

--- extensions/NexusKitten/corsproxy.js
+++ extensions/NexusKitten/corsproxy.js
@@ -4,59 +4,59 @@
 // By: NamelessCat <https://scratch.mit.edu/users/NexusKitten/>
 
 (function(Scratch) {
-  'use strict';
+  "use strict";
 
   if (!Scratch.extensions.unsandboxed) {
-    throw new Error('CORS Proxy must run unsandboxed');
+    throw new Error("CORS Proxy must run unsandboxed");
   }
 
-  var proxy = 'corsproxy';
+  var proxy = "corsproxy";
 
   // Provide 2 options in case one goes down, or something.
-  const corsproxy = 'https://corsproxy.io/?';
-  const allorigins = 'https://api.allorigins.win/raw?url=';
+  const corsproxy = "https://corsproxy.io/?";
+  const allorigins = "https://api.allorigins.win/raw?url=";
 
   class nkcorsproxy {
     getInfo() {
       return {
-        id: 'nkcorsproxy',
-        name: 'CORS Proxy',
+        id: "nkcorsproxy",
+        name: "CORS Proxy",
         docsURI: "https://extensions.turbowarp.org/CubesterYT/corsproxy",
-        color1: '#376661',
-        color2: '#3b6766',
+        color1: "#376661",
+        color2: "#3b6766",
         blocks: [
-          '---',
+          "---",
           {
-            opcode: 'addcors',
+            opcode: "addcors",
             blockType: Scratch.BlockType.REPORTER,
-            text: '[URL] CORS protected',
+            text: "[URL] CORS protected",
             arguments: {
               URL: {
                 type: Scratch.ArgumentType.STRING,
-                default: ''
+                default: ""
               }
             }
           },
           {
-            opcode: 'fetchcors',
+            opcode: "fetchcors",
             blockType: Scratch.BlockType.REPORTER,
-            text: 'fetch [URL] CORS protected',
+            text: "fetch [URL] CORS protected",
             arguments: {
               URL: {
                 type: Scratch.ArgumentType.STRING,
-                default: ''
+                default: ""
               }
             }
           },
-          '---',
+          "---",
           {
-            opcode: 'setcors',
+            opcode: "setcors",
             blockType: Scratch.BlockType.COMMAND,
-            text: 'change proxy to [PROXY]',
+            text: "change proxy to [PROXY]",
             arguments: {
               PROXY: {
                 type: Scratch.ArgumentType.STRING,
-                menu: 'PROXIES',
+                menu: "PROXIES",
                 default: proxy
               }
             }
@@ -65,28 +65,28 @@
         menus: {
           PROXIES: {
             acceptreporters: true,
-            items: ['corsproxy', 'allorigins']
+            items: ["corsproxy", "allorigins"]
           }
         }
       };
     }
 
     addcors(args) {
-      if (proxy === 'allorigins') {
+      if (proxy === "allorigins") {
         return allorigins + args.URL;
       }
       return corsproxy + encodeURIComponent(args.URL);
     }
 
     async fetchcors(args) {
-      if (proxy === 'allorigins') {
+      if (proxy === "allorigins") {
         return await Scratch.fetch(allorigins + args.URL)
           .then(r => r.text())
-          .catch(() => '');
+          .catch(() => "");
       }
       return await Scratch.fetch(corsproxy + encodeURIComponent(args.URL))
         .then(r => r.text())
-        .catch(() => '');
+        .catch(() => "");
     }
 
     setcors(args) {
