86fc11d41198b330352036d3dc17ae2eeaefcca4
SimonShiki <sinangentoo@gmail.com>
2023-09-10 13:06:35 +0000

    add scopeVar extension
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

---
+++ extensions/SimonShiki/scopeVar.js
@@ -0,0 +1,151 @@
+// Name: Scope Variables
+// ID: scopeVar
+// Description: Manage your data inside your stack (or substack).
+// By: SimonShiki <https://github.com/SimonShiki/>
+// Original: Skyhigh173
+
+(function (Scratch) {
+  "use strict";
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error(`
+            "Scope Variables" must be loaded in a unsandboxed environment.
+        `);
+  }
+  const { BlockType, ArgumentType, Cast } = Scratch;
+
+  class ScopeVar {
+    getInfo() {
+      return {
+        id: "scopeVar",
+        name: "Scope Variables",
+        collaborator: "Skyhigh173, SimonShiki",
+        description: "Manage your data inside.",
+        color1: "#9999FF",
+        blocks: [
+          {
+            opcode: "scope",
+            blockType: BlockType.COMMAND,
+            branchCount: 1,
+            text: "scope",
+          },
+          "---",
+          {
+            opcode: "create",
+            blockType: BlockType.COMMAND,
+            text: "create scoped [VAR] with [VALUE]",
+            arguments: {
+              VAR: {
+                type: ArgumentType.STRING,
+                defaultValue: "variable",
+              },
+              VALUE: {
+                type: ArgumentType.STRING,
+                defaultValue: "value",
+              },
+            },
+          },
+          {
+            opcode: "set",
+            blockType: BlockType.COMMAND,
+            text: "set scoped [VAR] to [VALUE]",
+            arguments: {
+              VAR: {
+                type: ArgumentType.STRING,
+                defaultValue: "variable",
+              },
+              VALUE: {
+                type: ArgumentType.STRING,
+                defaultValue: "value",
+              },
+            },
+          },
+          {
+            opcode: "get",
+            blockType: BlockType.REPORTER,
+            text: "get scoped [VAR]",
+            arguments: {
+              VAR: {
+                type: ArgumentType.STRING,
+                defaultValue: "variable",
+              },
+            },
+          },
+        ],
+      };
+    }
+
+    _getVarObjByName(name, thread) {
+      const { stackFrames } = thread;
+      // current block is top-level, read it from thread
+      if (stackFrames.length < 2) {
+        // initialize in thread
+        if (typeof thread.vars !== "object") {
+          thread.vars = {};
+        }
+        return thread.vars;
+      }
+
+      // look up the variable from outer scope up
+      for (let i = stackFrames.length - 2; i >= 0; i--) {
+        const { executionContext } = stackFrames[i];
+        // scope var hadn't been initialized in current stack frame
+        if (
+          executionContext === null ||
+          typeof executionContext.vars !== "object"
+        ) {
+          continue;
+        }
+        // current stack frame doesn't have an object with the name
+        if (!(name in executionContext.vars)) {
+          continue;
+        }
+        return executionContext.vars;
+      }
+
+      // return if it exists in top-level
+      if (typeof thread.vars === "object" && name in thread.vars) {
+        return thread.vars;
+      }
+
+      // initialize in outer scope
+      const outerStackFrame = stackFrames[stackFrames.length - 2];
+      return this._initScope(outerStackFrame);
+    }
+
+    _initScope(outerStackFrame) {
+      if (!outerStackFrame.executionContext) {
+        outerStackFrame.executionContext = {};
+      }
+      const { executionContext } = outerStackFrame;
+      if (typeof executionContext.vars !== "object") {
+        executionContext.vars = {};
+      }
+      return executionContext.vars;
+    }
+
+    scope(args, util) {
+      util.startBranch(1, false);
+    }
+
+    create(args, util) {
+      const varName = Cast.toString(args.var);
+      const outerStackFrame = stackFrames[stackFrames.length - 2];
+      const vars = this._initScope(outerStackFrame);
+      vars[varName] = Cast.toString(args.value);
+    }
+
+    set(args, util) {
+      const varName = Cast.toString(args.var);
+      const vars = this._getVarObjByName(varName, util.thread);
+      vars[varName] = Cast.toString(args.value);
+    }
+
+    get(args, util) {
+      const varName = Cast.toString(args.var);
+      const vars = this._getVarObjByName(varName, util.thread);
+      return vars[varName] ?? "";
+    }
+  }
+
+  Scratch.extensions.register(new ScopeVar());
+})(Scratch);
