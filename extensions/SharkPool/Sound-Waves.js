61087142f42457b82964c4f236502c8875c5f687
SharkPool-SP <139097378+SharkPool-SP@users.noreply.github.com>
2023-09-26 18:42:12 -0700

    Update Sound-Waves.js

--- extensions/SharkPool/Sound-Waves.js
+++ extensions/SharkPool/Sound-Waves.js
@@ -222,12 +222,14 @@
     }
 
     playNote({ WAVE, NOTE, DURATION, ID }) {
+      this.stopID({ ID });
       const realNote = this.convertScratchNoteToRealNote(Math.round(NOTE));
       const frequency = this.noteToFrequency(realNote);
       this.playSound(frequency, DURATION, WAVE, ID);
     }
 
     playNoteContinuously({ WAVE, NOTE, ID }) {
+      this.stopID({ ID });
       const realNote = this.convertScratchNoteToRealNote(Math.round(NOTE));
       const frequency = this.noteToFrequency(realNote);
       this.playingNote = true;
