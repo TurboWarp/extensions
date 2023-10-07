d432c78cb61b27c2341eed4458dde8e5bf23ad19
ClaytonTDM <claytontdm@gmail.com>
2023-10-07 02:15:42 -0400

    Add files via upload

---
+++ extensions/Clay/htmlEncode.js
@@ -0,0 +1,27 @@
+class HtmlEncode {
+  getInfo() {
+    return {
+      id: 'htmlencode',
+      name: 'HTML Encode',
+      blocks: [
+        {
+          opcode: 'htmlEncode',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Encode text [text]',
+          arguments: {
+            text: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'TurboWarp is awesome!'
+            }
+          }
+        }
+      ]
+    };
+  }
+
+  htmlEncode({ text }) {
+    return text.replace(/[\u00A0-\u9999<>\&]/g, i => '&#' + i.charCodeAt(0) + ';');
+  }
+}
+
+Scratch.extensions.register(new HtmlEncode());
