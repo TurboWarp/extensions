c4c3cf001e4f3c94701d5b50f5b119c2495d09a2
ClaytonTDM <claytontdm@gmail.com>
2023-10-07 02:32:44 -0400

    prettier again

--- extensions/Clay/htmlEncode.js
+++ extensions/Clay/htmlEncode.js
@@ -2,37 +2,37 @@
 // ID: clayhtmlencode
 // Description: Simple extension for encoding text in HTML escape codes.
 // By: ClaytonTDM
-(function(Scratch) {
-    "use strict";
+(function (Scratch) {
+  "use strict";
 
-    class HtmlEncode {
-        getInfo() {
-            return {
-                id: "htmlencode",
-                name: "HTML Encode",
-                blocks: [{
-                    opcode: "htmlEncode",
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: "Encode text [text]",
-                    arguments: {
-                        text: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: "TurboWarp is awesome!",
-                        },
-                    },
-                }, ],
-            };
-        }
-
-        htmlEncode({
-            text
-        }) {
-            return text.replace(
-                /[\u00A0-\u9999<>]/g,
-                (i) => "&#" + i.charCodeAt(0) + ";",
-            );
-        }
+  class HtmlEncode {
+    getInfo() {
+      return {
+        id: "htmlencode",
+        name: "HTML Encode",
+        blocks: [
+          {
+            opcode: "htmlEncode",
+            blockType: Scratch.BlockType.REPORTER,
+            text: "Encode text [text]",
+            arguments: {
+              text: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "TurboWarp is awesome!",
+              },
+            },
+          },
+        ],
+      };
     }
 
-    Scratch.extensions.register(new HtmlEncode());
+    htmlEncode({ text }) {
+      return text.replace(
+        /[\u00A0-\u9999<>]/g,
+        (i) => "&#" + i.charCodeAt(0) + ";"
+      );
+    }
+  }
+
+  Scratch.extensions.register(new HtmlEncode());
 })(Scratch);
