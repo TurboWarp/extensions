dd0860b180f3b33f41cbaec4d77b15743e7f4dde
Muffin <muffin@mailbox.org>
2023-11-05 18:10:43 -0600

    Fix some issues

--- extensions/Clay/htmlEncode.js
+++ extensions/Clay/htmlEncode.js
@@ -2,23 +2,27 @@
 // ID: clayhtmlencode
 // Description: Simple extension for encoding text in HTML escape codes.
 // By: ClaytonTDM
+
 (function (Scratch) {
   "use strict";
 
   class HtmlEncode {
     getInfo() {
       return {
-        id: "htmlencode",
+        id: "claytonhtmlencode",
         name: "HTML Encode",
         blocks: [
           {
-            opcode: "htmlEncode",
+            opcode: "encode",
             blockType: Scratch.BlockType.REPORTER,
-            text: "Encode text [text]",
+            text: "encode [text] as HTML-safe",
             arguments: {
               text: {
                 type: Scratch.ArgumentType.STRING,
-                defaultValue: "TurboWarp is awesome!",
+                // don't use a script tag as the example here as the closing script
+                // tag might break things when this extension gets inlined in packed
+                // projects
+                defaultValue: "<h1>Hello!</h1>",
               },
             },
           },
@@ -26,11 +30,23 @@
       };
     }
 
-    htmlEncode({ text }) {
-      return text.replace(
-        /[\u00A0-\u9999<>]/g,
-        (i) => "&#" + i.charCodeAt(0) + ";"
-      );
+    encode({ text }) {
+      return Scratch.Cast.toString(text).replace(/["'&<>]/g, (a) => {
+        switch (a) {
+          case "&":
+            return "&amp;";
+          case '"':
+            return "&apos;";
+          case "'":
+            return "&quot;";
+          case ">":
+            return "&gt;";
+          case "<":
+            return "&lt;";
+        }
+        // this should never happen...
+        return "";
+      });
     }
   }
 
