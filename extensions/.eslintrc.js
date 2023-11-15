7094ebce516ce69d0e53a7fc274707cdb4de007a
Muffin <muffin@mailbox.org>
2023-11-15 14:31:00 -0600

    Reject some invalid translate() calls

--- extensions/.eslintrc.js
+++ extensions/.eslintrc.js
@@ -88,6 +88,18 @@ module.exports = {
       {
         selector: 'CallExpression[callee.object.object.name=Scratch][callee.object.property.name=translate][callee.property.name=setup]',
         message: 'Do not call Scratch.translate.setup() yourself. Just use Scratch.translate() and let the build script handle it.'
+      },
+      {
+        selector: 'MethodDefinition[key.name=getInfo] Property[key.name=id][value.callee.property.name=translate]',
+        message: 'Do not translate extension ID'
+      },
+      {
+        selector: 'MethodDefinition[key.name=docsURI] Property[key.name=id][value.callee.property.name=translate]',
+        message: 'Do not translate docsURI'
+      },
+      {
+        selector: 'MethodDefinition[key.name=getInfo] Property[key.name=opcode][value.callee.property.name=translate]',
+        message: 'Do not translate block opcode'
       }
     ]
   }
