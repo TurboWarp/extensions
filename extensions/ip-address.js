4cd6422a701089d80168f657128e39e00ae46765
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2023-11-27 19:08:19 -0500

    Update ip-address.js

--- extensions/ip-address.js
+++ extensions/ip-address.js
@@ -3,10 +3,38 @@
 // Description: Blocks for handling public IP addresses
 // By: PPPDUD
 
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+
+*/
+
 (function(Scratch) {
   'use strict';
 
-class HelloWorld {
+class ipaddresses {
   getInfo() {
     return {
       id: 'ipaddresses',
@@ -17,8 +45,8 @@ class HelloWorld {
       blocks: [
         {
           opcode: 'get_ip',
-          blockType: Scratch.BlockType.REPORTER,
           text: 'get public ip address',
+          blockType: Scratch.BlockType.REPORTER,
 	  disableMonitor: true
         }
       ]
@@ -26,8 +54,7 @@ class HelloWorld {
   }
 
   get_ip() {
-    if (true) { // true is a placeholder for in case I choose to add a permissions system
-    	return fetch("https://api.ipify.org/")
+    	return Scratch.fetch("https://api.ipify.org/")
      	 .then((response) => {
      	   return response.text();
    	   })
@@ -37,12 +64,10 @@ class HelloWorld {
    	   });
  	 }
 	}
-}
 
-Scratch.extensions.register(new HelloWorld());
 
+  Scratch.extensions.register(new ipaddresses());
 })(Scratch);
-
 /*
 
 TODO:
