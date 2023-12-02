a5c6ab6ae34045c2b5e23a15bf05b815154eca3e
Muffin <muffin@mailbox.org>
2023-12-02 16:19:51 -0600

    npm run format

--- extensions/iframe.js
+++ extensions/iframe.js
@@ -79,8 +79,12 @@
       iframe.style.height = `${(effectiveHeight / stageHeight) * 100}%`;
 
       iframe.style.transform = "";
-      iframe.style.top = `${(0.5 - (effectiveHeight / 2 / stageHeight) - (y / stageHeight)) * 100}%`;
-      iframe.style.left = `${(0.5 - (effectiveWidth / 2 / stageWidth) + (x / stageWidth)) * 100}%`;
+      iframe.style.top = `${
+        (0.5 - effectiveHeight / 2 / stageHeight - y / stageHeight) * 100
+      }%`;
+      iframe.style.left = `${
+        (0.5 - effectiveWidth / 2 / stageWidth + x / stageWidth) * 100
+      }%`;
     }
   };
 
