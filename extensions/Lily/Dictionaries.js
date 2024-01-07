355f22cabcc2c78a2550a2d662cac26b9452a99a
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-01-07 20:04:13 +0000

    ACTUALLY disable monitor

--- extensions/Lily/Dictionaries.js
+++ extensions/Lily/Dictionaries.js
@@ -196,7 +196,7 @@
       if (safeName.length < 1)
         safeName += jsonEscape("/!\\ Invalid name, please check the console.");
       const mutation = mutationXML(
-        `{"blockType":"reporter","terminal":false,"blockAllThreads":false,"arguments":{"DICTIONARY":{"type":"string","defaultValue":"${uid}"}},"opcode":"getDictionary","text":"${safeName}","isDynamic":true,"hideFromPalette":false}`
+        `{"blockType":"reporter","terminal":false,"disableMonitor":true,"blockAllThreads":false,"arguments":{"DICTIONARY":{"type":"string","defaultValue":"${uid}"}},"opcode":"getDictionary","text":"${safeName}","isDynamic":true,"hideFromPalette":false}`
       );
       const reporter = `<block type="lmsDictionaries_getDictionary">${mutation}</block>`;
       xml[name] = reporter;
