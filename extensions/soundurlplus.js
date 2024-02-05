3213b94004b3b85ab1148bffb799a3546c6c974e
Xmer <108153683+XmerStudio@users.noreply.github.com>
2024-02-06 02:17:58 +0300

    Sound URL Plus - New Features
    Duration of Initialized Sound
    Adjustable] Switch to Initialized Audio Second
    Adjusting the Volume and Volume Value of the Initialized Sound (Works Separately from Other Sounds)
    Control of Initialized Audio
    
    A few more Advanced Features and blah blah blah :)

--- extensions/soundurlplus.js
+++ extensions/soundurlplus.js
@@ -1,11 +1,16 @@
 // Owner: TurboWarp
 // Edit: XmerStudios
 // Name: Sound URL Plus
-// v1.0.0
+// Version: 1.3.5
 
 class MusicExtension {
   constructor() {
     this.playingSounds = new Map();
+    this.currentSoundURL = '';
+    this.isPlaying = false;
+    this.errorStatus = '';
+    this.waitingPromise = null;
+    this.volume = 100;
   }
 
   getInfo() {
@@ -45,38 +50,201 @@ class MusicExtension {
           blockType: Scratch.BlockType.REPORTER,
           text: 'Current sound URL',
         },
+        {
+          opcode: 'isPlayingMusic',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'Is music playing?',
+        },
+        {
+          opcode: 'soundTime',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Sound Time',
+        },
+        {
+          opcode: 'soundCurrentTime',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Sound Current Time',
+        },
+        {
+          opcode: 'setVolume',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Set volume to [volume]',
+          arguments: {
+            volume: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 100,
+            },
+          },
+        },
+        {
+          opcode: 'getVolume',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Get volume',
+        },
+        {
+          opcode: 'seekSound',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Go to time [time] seconds in music',
+          arguments: {
+            time: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 0,
+            },
+          },
+        },
       ],
     };
   }
 
   playSound({ url }) {
-    const audio = new Audio(url);
-    audio.play();
-    this.playingSounds.set(url, audio);
+    try {
+      const testAudio = new Audio(url);
+      testAudio.addEventListener('canplaythrough', () => {
+        if (!this.isPlaying) {
+          const audio = new Audio(url);
+          audio.volume = this.volume / 100;
+          audio.addEventListener('ended', () => {
+            this.isPlaying = false;
+            this.currentSoundURL = '';
+            if (this.waitingPromise) {
+              this.waitingPromise.resolve();
+              this.waitingPromise = null;
+            }
+          });
+          audio.play();
+          this.playingSounds.set(url, audio);
+          this.currentSoundURL = url;
+          this.isPlaying = true;
+          this.errorStatus = '';
+        }
+      });
+
+      testAudio.addEventListener('error', () => {
+        this.errorControl();
+      });
+    } catch (error) {
+      console.error('Error playing sound:', error);
+      this.errorControl();
+    }
   }
 
-  playSoundAndWait({ url }) {
-    return new Promise((resolve) => {
-      const audio = new Audio(url);
-      audio.addEventListener('ended', () => {
-        resolve();
+  async playSoundAndWait({ url }) {
+    try {
+      return new Promise(async (resolve) => {
+        const testAudio = new Audio(url);
+        testAudio.addEventListener('canplaythrough', async () => {
+          if (!this.isPlaying) {
+            const audio = new Audio(url);
+            audio.volume = this.volume / 100;
+            audio.addEventListener('ended', () => {
+              this.isPlaying = false;
+              this.currentSoundURL = '';
+              resolve();
+            });
+            audio.play();
+            this.playingSounds.set(url, audio);
+            this.currentSoundURL = url;
+            this.isPlaying = true;
+            this.errorStatus = '';
+          } else {
+            if (this.waitingPromise) {
+              this.waitingPromise.resolve();
+              this.waitingPromise = null;
+            }
+            resolve();
+          }
+        });
+
+        testAudio.addEventListener('error', () => {
+          this.errorControl();
+          resolve();
+        });
+
+        this.waitingPromise = {
+          resolve,
+          reject: () => {},
+        };
       });
-      audio.play();
-      this.playingSounds.set(url, audio);
-    });
+    } catch (error) {
+      console.error('Error playing sound:', error);
+      this.errorControl();
+    }
   }
 
   stopSound() {
     this.playingSounds.forEach((audio) => {
       audio.pause();
       audio.currentTime = 0;
+      audio.removeEventListener('ended', () => {
+        this.isPlaying = false;
+        this.currentSoundURL = '';
+        if (this.waitingPromise) {
+          this.waitingPromise.resolve();
+          this.waitingPromise = null;
+        }
+      });
     });
     this.playingSounds.clear();
+    this.currentSoundURL = '';
+    this.isPlaying = false;
+    this.errorStatus = '';
+    if (this.waitingPromise) {
+      this.waitingPromise.resolve();
+      this.waitingPromise = null;
+    }
   }
 
   currentSound() {
-    const playingUrls = Array.from(this.playingSounds.keys());
-    return playingUrls.length > 0 ? playingUrls[0] : '';
+    return this.currentSoundURL;
+  }
+
+  soundTime() {
+    if (this.playingSounds.has(this.currentSoundURL)) {
+      return this.playingSounds.get(this.currentSoundURL).duration;
+    }
+    return 0;
+  }
+
+  soundCurrentTime() {
+    if (this.playingSounds.has(this.currentSoundURL)) {
+      if (this.isPlaying) {
+        return this.playingSounds.get(this.currentSoundURL).currentTime;
+      } else {
+        return '-';
+      }
+    }
+    return 0;
+  }
+
+  isPlayingMusic() {
+    return this.isPlaying;
+  }
+
+  seekSound({ time }) {
+    if (this.playingSounds.has(this.currentSoundURL)) {
+      this.playingSounds.get(this.currentSoundURL).currentTime = time;
+    }
+  }
+
+  setVolume({ volume }) {
+    this.volume = Math.min(100, Math.max(0, volume));
+    this.playingSounds.forEach((audio) => {
+      audio.volume = this.volume / 100;
+    });
+  }
+
+  getVolume() {
+    return this.volume;
+  }
+
+  errorControl() {
+    this.isPlaying = false;
+    this.currentSoundURL = '';
+    this.errorStatus = 'Error';
+    if (this.waitingPromise) {
+      this.waitingPromise.resolve();
+      this.waitingPromise = null;
+    }
   }
 }
 
