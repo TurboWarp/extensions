1fc89c0aad6e5092c64ef18dcf7705c80f05e00f
kingsuper195 <114847889+kingsuper195@users.noreply.github.com>
2024-02-24 17:29:41 +1100

    Update iframe.js

--- extensions/iframe.js
+++ extensions/iframe.js
@@ -4,8 +4,33 @@
 // Context: "iframe" is an HTML element that lets websites embed other websites.
 // License: MIT AND LGPL-3.0
 
-(function (Scratch) {
+/* generated l10n code */Scratch.translate.setup({"it":{"_It works!":"Funziona!","_close iframe":"chiudi iframe","_height":"altezza","_hide iframe":"nascondi iframe","_iframe [MENU]":"[MENU] dell'iframe","_interactive":"interattività","_resize behavior":"comportamento quando ridimensionato","_scale":"scala","_set iframe height to [HEIGHT]":"imposta larghezza dell'iframe a [HEIGHT]","_set iframe interactive to [INTERACTIVE]":"imposta interattività iframe a [INTERACTIVE]","_set iframe resize behavior to [RESIZE]":"imposta comportamento dell'iframe quando ridimensionato a [RESIZE]","_set iframe width to [WIDTH]":"imposta larghezza dell'iframe a [WIDTH]","_set iframe x position to [X]":"imposta posizione x dell'iframe a [X]","_set iframe y position to [Y]":"imposta posizione y dell'iframe a[Y]","_show HTML [HTML]":"mostra HTML [HTML]","_show iframe":"mostra iframe","_show website [URL]":"mostra sito [URL]","_visible":"visibilità","_width":"larghezza"},"ja":{"_Iframe":"埋め込み","_height":"高さ","_show HTML [HTML]":"[HTML]のHTMLを表示する","_show website [URL]":"[URL]のウェブサイトを表示する","_url":"URL","_width":"横幅"},"nb":{"_Iframe":"IFrame","_It works!":"Det fungerer!","_close iframe":"lukk iframe","_height":"høyde","_hide iframe":"skjul iframe","_interactive":"interaktiv","_resize behavior":"endre størrelsesoppførsel","_scale":"skala","_set iframe height to [HEIGHT]":"set iframe høyde til [HEIGHT]","_set iframe interactive to [INTERACTIVE]":"sett iframe interaktiv til [INTERACTIVE]","_set iframe resize behavior to [RESIZE]":"sett iframe resize oppførsel til [RESIZE]","_set iframe width to [WIDTH]":"set iframe bredde til [WIDTH]","_set iframe x position to [X]":"sett iframe x-posisjon til [X]","_set iframe y position to [Y]":"sett iframe y-posisjon til [Y]","_show HTML [HTML]":"vis HTML [HTML]","_show iframe":"vis iframe","_show website [URL]":"vis nettside [URL]","_viewport":"visningsområde","_visible":"synlig","_width":"bredde"},"nl":{"_It works!":"Het werkt!","_close iframe":"sluit iframe","_height":"hoogte","_hide iframe":"verberg iframe","_iframe [MENU]":"[MENU] van iframe","_interactive":"interactief?","_resize behavior":"formaatwijzigingsgedrag","_scale":"speelveld","_set iframe height to [HEIGHT]":"maak hoogte van iframe [HEIGHT]","_set iframe interactive to [INTERACTIVE]":"maak iframe interactief? [INTERACTIVE]","_set iframe resize behavior to [RESIZE]":"maak formaatwijzigingsgedrag van iframe [RESIZE]","_set iframe width to [WIDTH]":"maak breedte van iframe [WIDTH]","_set iframe x position to [X]":"maak x-positie van iframe [X]","_set iframe y position to [Y]":"maak y-positie van iframe [Y]","_show HTML [HTML]":"toon HTML [HTML]","_show iframe":"toon iframe","_show website [URL]":"toon website [URL]","_viewport":"beeldscherm","_visible":"zichtbaar?","_width":"breedte"},"ru":{"_It works!":"Работает!","_close iframe":"закрыть iframe","_height":"высота","_hide iframe":"спрятать iframe","_set iframe x position to [X]":"задать позицию iframe x в [X]","_set iframe y position to [Y]":"задать позицию iframe y в [Y]","_show HTML [HTML]":"показать HTML [HTML]","_show iframe":"показать iframe","_show website [URL]":"показать вебсайт [URL]","_visible":"виден","_width":"ширина"},"zh-cn":{"_Iframe":"内嵌框架","_It works!":"它在工作！","_close iframe":"退出内嵌网页","_height":"高度","_hide iframe":"隐藏内嵌网页","_iframe [MENU]":"内嵌网页的[MENU]","_set iframe height to [HEIGHT]":"设置内嵌网页的高度为[HEIGHT]","_set iframe width to [WIDTH]":"设置内嵌网页的宽度为[WIDTH]","_set iframe x position to [X]":"设置内嵌网页的x坐标为[X]","_set iframe y position to [Y]":"设置内嵌网页的y坐标为[Y]","_show HTML [HTML]":"显示来自文本[HTML]的网页","_show iframe":"显示内嵌网页","_show website [URL]":"显示来自URL[URL]的网页","_url":"URL","_visible":"显示状态","_width":"宽度"}});/* end generated l10n code */(function (Scratch) {
   "use strict";
+
+  const defaultIframe = `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PostMessageChild</title>
+    <script>
+    window.onload = function() {
+        document.getElementById('click').addEventListener('click', function() {
+            parent.postMessage(msg.value, '*');
+        });
+      }
+      window.addEventListener('message', function(event) {
+        rmsg.value = event.data;
+      });
+</script>
+</head>
+<body>
+    <input type=text id=msg><input type=button id=click value=gsso>
+    <input type=text id=rmsg>
+</body>
+</html>
+
+  `;
 
   /** @type {HTMLIFrameElement|null} */
   let iframe = null;
@@ -53,6 +78,7 @@
   let height = -1; // negative means default
   let interactive = true;
   let resizeBehavior = "scale";
+  let lastMessageFromChild = "";
 
   const updateFrameAttributes = () => {
     if (!iframe) {
@@ -109,6 +135,11 @@
     iframe.setAttribute("allowtransparency", "true");
     iframe.setAttribute("src", src);
 
+    window.addEventListener('message', function(event) {
+      lastMessageFromChild = event.data;
+      Scratch.vm.runtime.startHats('iframe_whenMessageReceivedFromChild');
+    });
+
     overlay = Scratch.renderer.addOverlay(iframe, getOverlayMode());
     updateFrameAttributes();
   };
@@ -149,7 +180,7 @@
             arguments: {
               HTML: {
                 type: Scratch.ArgumentType.STRING,
-                defaultValue: `<h1>${Scratch.translate("It works!")}</h1>`,
+                defaultValue: defaultIframe,
               },
             },
           },
@@ -247,6 +278,33 @@
               },
             },
           },
+          {
+            opcode: "postMessage",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("post [MESSAGE] to child"),
+            arguments: {
+              MESSAGE: {
+                type: Scratch.ArgumentType.STRING,
+              },
+            },
+          },
+          {
+            blockType: Scratch.BlockType.EVENT,
+            opcode: 'whenMessageReceivedFromChild',
+            text: 'when message is received from child',
+            isEdgeActivated: false, // required boilerplate
+            // arguments: {
+            //   MESSAGE: {
+            //     type: Scratch.ArgumentType.STRING,
+            //   },
+            // },
+          },
+          {
+            blockType: Scratch.BlockType.REPORTER,
+            opcode: 'messageFromChild',
+            text: 'message from child',
+
+          }
         ],
         menus: {
           getMenu: {
@@ -296,12 +354,13 @@
 
     async displayHTML({ HTML }) {
       closeFrame();
-      const url = `data:text/html;,${encodeURIComponent(
+      const url = `about:blank`; /* data:text/html;,${encodeURIComponent(
         Scratch.Cast.toString(HTML)
-      )}`;
-      if (await Scratch.canEmbed(url)) {
+      )}`;*/
+      // if (await Scratch.canEmbed(url)) {
         createFrame(url);
-      }
+        iframe.srcdoc = HTML;
+      // }
     }
 
     show() {
@@ -342,6 +401,14 @@
       } else {
         return "";
       }
+    }
+
+    postMessage({ MESSAGE }) {
+      iframe.contentWindow.postMessage(MESSAGE);
+    }
+
+    messageFromChild() {
+      return lastMessageFromChild;
     }
 
     setX({ X }) {
