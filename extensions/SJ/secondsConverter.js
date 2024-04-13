5c120239bdf5a1ceaef3c1de75ea1126703ff19c
Hiruja Edurapola <103011661+HirujaEdurapola@users.noreply.github.com>
2024-04-13 12:55:07 +0530

    Added some more conversations and decimal support

--- extensions/SJ/secondsConverter.js
+++ extensions/SJ/secondsConverter.js
@@ -20,7 +20,40 @@
           arguments: {
             SECONDS: {
               type: Scratch.ArgumentType.NUMBER,
-              defaultValue: 3663,
+              defaultValue: 3600,
+            },
+          },
+        },
+        {
+          opcode: "minutesToHours",
+          blockType: Scratch.BlockType.REPORTER,
+          text: "convert [MINUTES] minutes to hours",
+          arguments: {
+            MINUTES: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 120,
+            },
+          },
+        },
+        {
+          opcode: "minutesToSeconds",
+          blockType: Scratch.BlockType.REPORTER,
+          text: "convert [MINUTES] minutes to seconds",
+          arguments: {
+            MINUTES: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 2,
+            },
+          },
+        },
+        {
+          opcode: "hoursToSeconds",
+          blockType: Scratch.BlockType.REPORTER,
+          text: "convert [HOURS] hours to seconds",
+          arguments: {
+            HOURS: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 2,
             },
           },
         },
@@ -29,7 +62,7 @@
   };
 
   Extension.prototype.convertSeconds = function (args) {
-    var totalSeconds = Math.floor(args.SECONDS);
+    var totalSeconds = args.SECONDS;
     var hours = Math.floor(totalSeconds / 3600);
     var minutes = Math.floor((totalSeconds % 3600) / 60);
     var seconds = totalSeconds % 60;
@@ -44,6 +77,28 @@
     timeString += (seconds < 10 ? "0" : "") + seconds;
 
     return timeString;
+  };
+
+  Extension.prototype.minutesToHours = function (args) {
+    var totalMinutes = args.MINUTES;
+    var hours = Math.floor(totalMinutes / 60);
+    var minutes = totalMinutes % 60;
+
+    return hours + ":" + (minutes < 10 ? "0" : "") + minutes;
+  };
+
+  Extension.prototype.minutesToSeconds = function (args) {
+    var totalMinutes = args.MINUTES;
+    var seconds = totalMinutes * 60;
+
+    return seconds;
+  };
+
+  Extension.prototype.hoursToSeconds = function (args) {
+    var totalHours = args.HOURS;
+    var seconds = totalHours * 3600;
+
+    return seconds;
   };
 
   Scratch.extensions.register(new Extension());
