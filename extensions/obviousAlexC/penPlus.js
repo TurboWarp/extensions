7e331e9204c30b845e4939b74c21e765ecac29d3
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-05-06 15:53:32 -0400

    Preview

--- extensions/obviousAlexC/penPlus.js
+++ extensions/obviousAlexC/penPlus.js
@@ -40,6 +40,7 @@
       type: gl.UNSIGNED_BYTE,
       min: gl.LINEAR,
       wrap: gl.CLAMP_TO_EDGE,
+      premultiplyAlpha: true,
     },
     { format: gl.DEPTH_STENCIL },
   ];
@@ -151,7 +152,8 @@
                     void main()
                     {
                       gl_FragColor = v_color;
-                      gl_FragColor.rgb *= gl_FragColor.a;
+                      //gl_FragColor.rgb *= gl_FragColor.a;
+                      gl_FragColor.rgb = clamp(gl_FragColor.rgb / (gl_FragColor.a + 1e-3), 0.0, 1.0);
                       if (gl_FragColor.a == 0.0) {
                         discard;
                       }
@@ -227,7 +229,8 @@
                     void main()
                     {
                       gl_FragColor = texture2D(u_drawTex, v_texCoord);
-                      gl_FragColor.rgb *= gl_FragColor.a;
+                      //gl_FragColor.rgb *= gl_FragColor.a;
+                      gl_FragColor.rgb = clamp(gl_FragColor.rgb / (gl_FragColor.a + 1e-3), 0.0, 1.0);
                     }
                 `,
       },
