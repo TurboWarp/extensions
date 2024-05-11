0f8d972603177eadfbd75f0b2956c493b1a8289f
Xmer <108153683+XmerStudio@users.noreply.github.com>
2024-05-11 19:40:34 +0300

    New extension: Close Control (#1384)
    Co-authored-by: Muffin <muffin@muffin.ink>

---
+++ extensions/XmerOriginals/closecontrol.js
@@ -0,0 +1,69 @@
+// Name: Close Control
+// ID: xmerclosecontrol
+// Description: Ask before closing the tab.
+// By: XmerOriginals
+// License: MPL-2.0
+
+(function (Scratch) {
+  "use strict";
+
+  let enabled = false;
+
+  window.addEventListener("beforeunload", (e) => {
+    if (enabled) {
+      e.preventDefault();
+    }
+  });
+
+  class CloseControl {
+    getInfo() {
+      return {
+        id: "xmerclosecontrol",
+        name: Scratch.translate("Close Control"),
+        blocks: [
+          {
+            opcode: "setControl",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("set ask before closing tab to [OPTION]"),
+            arguments: {
+              OPTION: {
+                type: Scratch.ArgumentType.STRING,
+                menu: "option",
+              },
+            },
+          },
+          {
+            opcode: "getControl",
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: Scratch.translate("is close control enabled?"),
+          },
+        ],
+        menus: {
+          option: {
+            acceptReporters: true,
+            items: [
+              {
+                text: Scratch.translate("enabled"),
+                value: "true",
+              },
+              {
+                text: Scratch.translate("disabled"),
+                value: "false",
+              },
+            ],
+          },
+        },
+      };
+    }
+
+    setControl({ OPTION }) {
+      enabled = Scratch.Cast.toBoolean(OPTION);
+    }
+
+    getControl() {
+      return enabled;
+    }
+  }
+
+  Scratch.extensions.register(new CloseControl());
+})(Scratch);
