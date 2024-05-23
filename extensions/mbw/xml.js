9e222c7dfea46fd2566d2e46c17287a63eda643e
Procybit <kyleusnaff@gmail.com>
2024-05-23 17:54:52 +0300

    Update xml.js: implement setTextContent

--- extensions/mbw/xml.js
+++ extensions/mbw/xml.js
@@ -4,7 +4,7 @@
 // By: mybearworld <https://scratch.mit.edu/users/mybearworld/>
 // License: MIT
 
-(function (Scratch) {
+/* generated l10n code */Scratch.translate.setup({"nb":{"_add child [CHILD] to [XML]":"legg til barnet [CHILD] til [XML]","_attribute [ATTR] of [XML]":"attributt [ATTR] av [XML]","_attributes of [XML]":"attributter av [XML]","_child #[NO] of [XML]":"barn #[NO] av [XML]","_children amount of [XML]":"antall barn av [XML]","_does [XML] have attribute [ATTR]?":"Har [XML] attributt [ATTR]?","_does [XML] have children?":"Har [XML] barn?","_error message of [MAYBE_XML]":"feilmelding av [MAYBE_XML]","_is [MAYBE_XML] valid XML?":"er [MAYBE_XML] gyldig XML?","_query [QUERY] on [XML]":"spørr [QUERY] på [XML]","_query [QUERY] on [XML] matches?":"spørsmål [QUERY] på [XML] matcher?","_query all [QUERY] on [XML]":"spørr alle [QUERY] på [XML]","_remove attribute [ATTR] of [XML]":"fjern attributtet [ATTR] av [XML]","_remove child #[NO] of [XML]":"fjern barn #[NO] av [XML]","_replace child #[NO] of [XML] with [CHILD]":"erstatt barn #[NO] av [XML] med [CHILD]","_set attribute [ATTR] of [XML] to [VALUE]":"sett attributt [ATTR] av [XML] til [VALUE]","_tag name of [XML]":"tag navn av [XML]","_text of [XML]":"tekst av [XML]","_set text of [XML] to [VALUE]":"sett tekst av [XML] til [VALUE]"},"nl":{"_add child [CHILD] to [XML]":"voeg onderliggende [CHILD] toe aan [XML]","_attribute [ATTR] of [XML]":"attribuut [ATTR] van [XML]","_attributes of [XML]":"attributen van [XML]","_child #[NO] of [XML]":"onderliggende #[NO] van [XML]","_children amount of [XML]":"aantal onderliggenden van [XML]","_does [XML] have attribute [ATTR]?":"[XML] bevat attribuut [ATTR]?","_does [XML] have children?":"[XML] bevat onderliggenden?","_error message of [MAYBE_XML]":"foutmelding van [MAYBE_XML]","_is [MAYBE_XML] valid XML?":"is [MAYBE_XML] geldige XML?","_query [QUERY] on [XML]":"query [QUERY] op [XML]","_query [QUERY] on [XML] matches?":"query [QUERY] op [XML] komt overeen?","_query all [QUERY] on [XML]":"query alle [QUERY] op [XML]","_remove attribute [ATTR] of [XML]":"verwijder attribuut [ATTR] van [XML]","_remove child #[NO] of [XML]":"verwijder onderliggende #[NO] van [XML]","_replace child #[NO] of [XML] with [CHILD]":"vervang onderliggende #[NO] van [XML] door [CHILD]","_set attribute [ATTR] of [XML] to [VALUE]":"stel attribuut [ATTR] van [XML] in op [VALUE]","_tag name of [XML]":"tagnaam van [XML]","_text of [XML]":"tekst van [XML]","_set text of [XML] to [VALUE]":"stel tekst van [XML] in op [VALUE]"},"ru":{"_add child [CHILD] to [XML]":"добавить ребёнка [CHILD] файлу [XML]","_attribute [ATTR] of [XML]":"атрибут [ATTR] файла [XML]","_attributes of [XML]":"атрибуты [XML]","_child #[NO] of [XML]":"#[NO] ребёнок файла [XML]","_children amount of [XML]":"количество детей файла [XML]","_does [XML] have attribute [ATTR]?":"[XML] имеет атрибут [ATTR]?","_does [XML] have children?":"[XML] имеет ребёнка?","_error message of [MAYBE_XML]":"ошибка сообщения [MAYBE_XML]","_is [MAYBE_XML] valid XML?":"[MAYBE_XML] правильный XML файл?","_query [QUERY] on [XML]":"запрос [QUERY] на [XML]","_query [QUERY] on [XML] matches?":"запрос [QUERY] на [XML] совпадает?","_query all [QUERY] on [XML]":"запрос всех [QUERY] на [XML]","_remove attribute [ATTR] of [XML]":"удалить атрибут [ATTR] файла [XML]","_remove child #[NO] of [XML]":"удалить #[NO] ребёнка файла [XML]","_replace child #[NO] of [XML] with [CHILD]":"заменить ребёнка  под номером #[NO] файла [XML] на [CHILD]","_set attribute [ATTR] of [XML] to [VALUE]":"установить атрибут [ATTR] XML файла [XML] в [VALUE]","_tag name of [XML]":"имя тега [XML]","_text of [XML]":"текст [XML]","_set text of [XML] to [VALUE]":"установить текст XML файла [XML] в [VALUE]"},"zh-cn":{"_add child [CHILD] to [XML]":"[XML]添加子元素[CHILD]","_attribute [ATTR] of [XML]":"[XML]中属性[ATTR]的值","_attributes of [XML]":"[XML]的所有属性","_child #[NO] of [XML]":"[XML]第[NO]个子元素","_children amount of [XML]":"[XML]的子元素数量","_does [XML] have attribute [ATTR]?":"[XML]有属性[ATTR]吗？","_does [XML] have children?":"[XML]有子元素吗？","_error message of [MAYBE_XML]":"[MAYBE_XML]的错误信息","_is [MAYBE_XML] valid XML?":"[MAYBE_XML]是合法 XML？","_query [QUERY] on [XML]":"[XML]中第一个匹配[QUERY]的元素","_query [QUERY] on [XML] matches?":"[XML]能匹配[QUERY]吗？","_query all [QUERY] on [XML]":"[XML]中所有匹配[QUERY]的元素","_remove attribute [ATTR] of [XML]":"删除[XML]的属性[ATTR]","_remove child #[NO] of [XML]":"删除[XML]第[NO]个子元素","_replace child #[NO] of [XML] with [CHILD]":"将[XML]第[NO]子元素替换为[CHILD]","_set attribute [ATTR] of [XML] to [VALUE]":"设置[XML]的属性[ATTR]为[VALUE]","_tag name of [XML]":"[XML]的标签名称","_text of [XML]":"[XML]的文本","_set text of [XML] to [VALUE]":"将[XML]的文本设置为[VALUE]"}});/* end generated l10n code */(function (Scratch) {
   "use strict";
 
   class XML {
@@ -82,6 +82,21 @@
               XML: {
                 type: Scratch.ArgumentType.STRING,
                 defaultValue: "<hello>world</hello>",
+              },
+            },
+          },
+          {
+            opcode: "setTextContent",
+            blockType: Scratch.BlockType.REPORTER,
+            text: Scratch.translate("set text of [XML] to [VALUE]"),
+            arguments: {
+              XML: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "<hello>world</hello>",
+              },
+              VALUE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "world!",
               },
             },
           },
@@ -340,6 +355,20 @@
         return "";
       }
       return xml.textContent;
+    }
+
+    /**
+     * @param {object} args
+     * @param {unknown} args.XML
+     * @param {unknown} args.VALUE
+     */
+    setTextContent({ XML, VALUE }) {
+      const { xml } = this.stringToXml(Scratch.Cast.toString(XML));
+      if (xml === null) {
+        return "";
+      }
+	  xml.textContent = VALUE;
+      return this.xmlToString(xml);
     }
 
     /**
