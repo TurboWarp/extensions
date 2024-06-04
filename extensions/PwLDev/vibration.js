3401e606e35c4442192a3cf082ea1130ffc43962
PWL / SmartElectron <pgibran09@gmail.com>
2024-06-04 02:03:57 -0700

    Create extension

---
+++ extensions/PwLDev/vibration.js
@@ -0,0 +1,63 @@
+// Name: Vibration
+// ID: pwldevvibration
+// Description: Access and control the device's vibration. Only available in some devices.
+// By: PwLDev <https://scratch.mit.edu/users/PwLDev/>
+// License: MPL-2.0
+
+(function (Scratch) {
+    'use strict';
+
+    if (!Scratch.extensions.unsandboxed) {
+        throw new Error('This extension must run unsandboxed in order to work.');
+    }
+
+    class Vibration {
+        getInfo() {
+            return {
+                id: "pwldevvibration",
+                name: "Vibration",
+                color1: "#45a15c",
+                color2: "#317041",
+                color3: "#35523c",
+                blocks: [
+                    {
+                        opcode: "startVibration",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "start vibration"
+                    },
+                    {
+                        opcode: "stopVibration",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "stop vibrations"
+                    },
+                    '---',
+                    {
+                        opcode: "timedVibration",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "vibrate for [SECONDS] seconds",
+                        arguments: {
+                            SECONDS: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 2
+                            }
+                        },
+                    }
+                ]
+            }
+        }
+    
+        startVibration() {
+            navigator.vibrate(5000);
+        }
+
+        stopVibration() {
+            navigator.vibrate(0);
+        }
+
+        timedVibration(args) {
+            navigator.vibrate(args.SECONDS * 1000);
+        }
+    }
+    
+    Scratch.extensions.register(new Vibration());
+})(Scratch);
