eb7df9e76995d1a14c6a4f771acb69afb2ee5c33
GarboMuffin <muffin@muffin.ink>
2024-06-11 12:30:38 -0500

    Lily/AllMenus: hide unused More Events menu and XML escape (#1537)
    Related to https://github.com/TurboWarp/extensions/issues/1530
    
    XML escaping prevents extension from breaking if menu opcodes have weird
    characters but should not have any actual security impact since this is
    Blockly XML. It doesn't get fed directly into innerHTML = ...

--- extensions/Lily/AllMenus.js
+++ extensions/Lily/AllMenus.js
@@ -7,28 +7,53 @@
 (function (Scratch) {
   "use strict";
 
-  var blockXML;
-  const blacklist = ["looks_costumenumbername", "extension_wedo_tilt_menu"];
+  let blockXML;
 
-  Scratch.vm.addListener("BLOCKSINFO_UPDATE", refreshMenus);
+  const blocklist = [
+    "looks_costumenumbername",
+    "extension_wedo_tilt_menu",
 
-  function refreshMenus() {
+    // Unused menu in More Events that won't be translated
+    "lmsMoreEvents_menu_state",
+  ];
+
+  const escapeXML = (text) =>
+    text.replace(/["'&<>]/g, (i) => {
+      switch (i) {
+        case "&":
+          return "&amp;";
+        case '"':
+          return "&apos;";
+        case "'":
+          return "&quot;";
+        case ">":
+          return "&gt;";
+        case "<":
+          return "&lt;";
+      }
+      return "";
+    });
+
+  const refreshMenus = () => {
     if (!window.ScratchBlocks) return;
     Scratch.vm.removeListener("BLOCKSINFO_UPDATE", refreshMenus);
 
     let allBlocks = Object.keys(ScratchBlocks.Blocks);
 
     allBlocks = allBlocks.filter(
-      (item) => item.includes("menu") && !blacklist.includes(item)
+      (item) => item.includes("menu") && !blocklist.includes(item)
     );
 
     const menuBlocks = allBlocks.map(
-      (item) => '<block id="' + item + '" type="' + item + '"/>'
+      (item) =>
+        '<block id="' + escapeXML(item) + '" type="' + escapeXML(item) + '"/>'
     );
 
     blockXML = menuBlocks.join("");
     Scratch.vm.runtime.extensionManager.refreshBlocks();
-  }
+  };
+
+  Scratch.vm.addListener("BLOCKSINFO_UPDATE", refreshMenus);
 
   class AllMenus {
     constructor() {
