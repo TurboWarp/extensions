2ea5a85c72da092a6e203a5ab92578191a440eee
0832k12 <0832k12@gmail.com>
2024-06-16 16:19:55 +0800

    Upload

---
+++ extensions/0832/projectcover.js
@@ -0,0 +1,52 @@
+// Name: ProjectCover
+// ID: projectcover
+// Description: Load another work when loading it (?
+// License: MIT
+
+(function (Scratch) {
+  'use strict';
+  'masmuruang';
+
+  class Cover {
+    constructor() {
+      if (!Scratch.vm.runtime.extensionStorage.requireproject) {
+        Scratch.vm.runtime.extensionStorage.requireproject = {};
+        this.storage = Scratch.vm.runtime.extensionStorage.requireproject;
+      }
+      else {
+        this.storage = Scratch.vm.runtime.extensionStorage.requireproject;
+        this.load();
+      }
+    }
+    getInfo() {
+      return {
+        id: 'projectcover',
+        name: 'ProjectCover',
+        color1: '#f54242',
+        color2: '#f54242',
+        color3: '#f54242',
+        blocks: [
+          {
+            opcode: 'require',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'require'
+          }
+        ]
+      };
+    }
+    require() {
+      this.storage.URL = prompt('URL: ');
+    }
+    load() {
+      fetch(this.storage.URL)
+        .then((r) => r.arrayBuffer())
+        .then((buffer) => Scratch.vm.loadProject(buffer))
+        .then(() => { })
+        .catch((error) => {
+          console.log("Error", error);
+        });
+    }
+  }
+
+  Scratch.extensions.register(new Cover());
+})(Scratch);
