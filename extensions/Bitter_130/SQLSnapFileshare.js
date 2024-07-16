9d33fbb1eee2558130c477fbf9f78b51fd0c6596
Cubester <cubester.spamlegends@gmail.com>
2024-07-15 22:45:19 -0400

    Reformat, make Unsandboxed, and Translation Functions

--- extensions/Bitter_130/SQLSnapFileshare.js
+++ extensions/Bitter_130/SQLSnapFileshare.js
@@ -1,150 +1,160 @@
-class ServerExtension {
-  constructor(runtime) {
-    this.runtime = runtime;
-    this.serverURL = 'https://snapextensions.uni-goettingen.de/handleTextfile.php';
-  }
+// Name: SQLSnap! Fileshare
+// ID: bitter130SQLSnapFileshare
+// Description: Port of the SQLSnap! Fileshare extension for cloud file share utilization.
+// By: Bitter_130
 
-  getInfo() {
-    return {
-      id: 'serverData',
-      name: 'SQLSnap! Servers',
-      color1: '#31b3d4',
-      color2: '#179fc2',
-      blocks: [
-        {
-          opcode: 'setServerURL',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'set server URL to [url]',
-          arguments: {
-            url: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: this.serverURL,
+(function (Scratch) {
+  "use strict";
+
+  class SQLSnapFileshare {
+    constructor() {
+      this.serverURL =
+        "https://snapextensions.uni-goettingen.de/handleTextfile.php";
+    }
+
+    getInfo() {
+      return {
+        id: "bitter130SQLSnapFileshare",
+        name: "SQLSnap! " + Scratch.translate("Fileshare"),
+        color1: "#31b3d4",
+        color2: "#179fc2",
+        blocks: [
+          {
+            opcode: "setServerURL",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("set server URL to [URL]"),
+            arguments: {
+              URL: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: this.serverURL,
+              },
             },
           },
-        },
-        {
-          opcode: 'saveToServer',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'save [variableName] with content [content]',
-          arguments: {
-            variableName: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'default.txt',
-            },
-            content: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Hello, World!',
-            },
-          },
-        },
-        {
-          opcode: 'loadFromServer',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'load [variableName]',
-          arguments: {
-            variableName: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'data.txt',
+          {
+            opcode: "saveToServer",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("save [FILE] with content [CONTENT]"),
+            arguments: {
+              FILE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: Scratch.translate("data") + ".txt",
+              },
+              CONTENT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: Scratch.translate("Hello World!"),
+              },
             },
           },
-        },
-        {
-          opcode: 'fits',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: '[data] fits on server?',
-          arguments: {
-            data: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'data',
+          {
+            opcode: "loadFromServer",
+            blockType: Scratch.BlockType.REPORTER,
+            text: Scratch.translate("load [FILE]"),
+            arguments: {
+              FILE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: Scratch.translate("data") + ".txt",
+              },
             },
           },
-        },
-        {
-          opcode: 'deleteFromServer',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'delete [variableName]',
-          arguments: {
-            variableName: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'data.txt',
+          {
+            opcode: "fits",
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: Scratch.translate("[DATA] fits on server?"),
+            arguments: {
+              DATA: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: Scratch.translate("data"),
+              },
             },
           },
-        },
-      ],
-    };
+          {
+            opcode: "deleteFromServer",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("delete [FILE]"),
+            arguments: {
+              FILE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: Scratch.translate("data") + ".txt",
+              },
+            },
+          },
+        ],
+      };
+    }
+
+    setServerURL(args) {
+      args.URL = Scratch.Cast.toString(args.URL);
+      this.serverURL = args.URL;
+    }
+
+    saveToServer(args) {
+      args.FILE = Scratch.Cast.toString(args.FILE);
+      args.CONTENT = Scratch.Cast.toString(args.CONTENT);
+
+      const url =
+        this.serverURL +
+        "?type=write" +
+        "&content=" +
+        encodeURIComponent(args.CONTENT) +
+        "&filename=./textfiles/" +
+        encodeURIComponent(args.FILE);
+
+      return fetch(url)
+        .then((response) => response.text())
+        .then((result) => result === "ok")
+        .catch((error) => {
+          console.error("Failed to save data:", error);
+          return false;
+        });
+    }
+
+    loadFromServer(args) {
+      args.FILE = Scratch.Cast.toString(args.FILE);
+
+      const url =
+        this.serverURL +
+        "?type=read" +
+        "&filename=./textfiles/" +
+        encodeURIComponent(args.FILE);
+
+      return fetch(url)
+        .then((response) => response.text())
+        .then((text) => {
+          if (text === "ERROR: file does not exist") {
+            return ""; // Return empty string or any indication that file does not exist
+          } else {
+            return text;
+          }
+        })
+        .catch((error) => {
+          console.error("Failed to load data:", error);
+          return "can't get data";
+        });
+    }
+
+    fits(args) {
+      args.DATA = Scratch.Cast.toString(args.DATA);
+      return args.DATA.length < 10001;
+    }
+
+    deleteFromServer(args) {
+      args.FILE = Scratch.Cast.toString(args.FILE);
+
+      const url =
+        this.serverURL +
+        "?type=delete" +
+        "&filename=./textfiles/" +
+        encodeURIComponent(args.FILE);
+
+      return fetch(url)
+        .then((response) => response.text())
+        .then((result) => result === "ok")
+        .catch((error) => {
+          console.error("Failed to delete data:", error);
+          return false;
+        });
+    }
   }
 
-  setServerURL(args, util) {
-    this.serverURL = args.url;
-    console.log(`Server URL set to: ${this.serverURL}`); // Debug line
-  }
-
-  saveToServer(args, util) {
-    const variableName = args.variableName;
-    const content = args.content;
-
-    const url =
-      this.serverURL +
-      '?type=write' +
-      '&content=' +
-      encodeURIComponent(content) +
-      '&filename=./textfiles/' +
-      encodeURIComponent(variableName);
-
-    return fetch(url)
-      .then(response => response.text())
-      .then(result => (result === 'ok'))
-      .catch(error => {
-        console.error('Failed to save data:', error);
-        return false;
-      });
-  }
-
-  loadFromServer(args, util) {
-    const variableName = args.variableName;
-
-    const url =
-      this.serverURL +
-      '?type=read' +
-      '&filename=./textfiles/' +
-      encodeURIComponent(variableName);
-
-    return fetch(url)
-      .then(response => response.text())
-      .then(text => {
-        if (text === 'ERROR: file does not exist') {
-          return ''; // Return empty string or any indication that file does not exist
-        } else {
-          return text;
-        }
-      })
-      .catch(error => {
-        console.error('Failed to load data:', error);
-        return "can't get data";
-      });
-  }
-
-  fits(args, util) {
-    return args.data.length < 10001;
-  }
-
-  deleteFromServer(args, util) {
-    const variableName = args.variableName;
-
-    const url =
-      this.serverURL +
-      '?type=delete' +
-      '&filename=./textfiles/' +
-      encodeURIComponent(variableName);
-
-    return fetch(url)
-      .then(response => response.text())
-      .then(result => (result === 'ok'))
-      .catch(error => {
-        console.error('Failed to delete data:', error);
-        return false;
-      });
-  }
-}
-
-Scratch.extensions.register(new ServerExtension());
+  Scratch.extensions.register(new SQLSnapFileshare());
+})(Scratch);
