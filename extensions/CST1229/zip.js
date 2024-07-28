c8513776afed0aa1fe6d993cf0f55f58da4ac056
CST1229 <68464103+CST1229@users.noreply.github.com>
2024-07-28 19:11:14 +0200

    cst1229/zip: expose extension object

--- extensions/CST1229/zip.js
+++ extensions/CST1229/zip.js
@@ -21,6 +21,10 @@
       this.zipPaths = Object.create(null);
       this.zip = null;
 
+      // for developers who want to integrate their extensions with this one
+      // @ts-ignore
+      Scratch.vm.runtime.ext_cst1229zip = this;
+
       this.zipError = false;
 
       Scratch.vm.runtime.on("RUNTIME_DISPOSED", () => {
