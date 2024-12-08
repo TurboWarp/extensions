4e17fdb2bd712e30c4bd23d1339c5a8e6518cfce
GoodBoi19 <181642507+Thebloxers998@users.noreply.github.com>
2024-12-08 20:57:57 +0000

    Update LoginNexus.js

--- extensions/LoginNexus.js
+++ extensions/LoginNexus.js
@@ -56,7 +56,7 @@
                     {
                         opcode: 'registerUser',
                         blockType: Scratch.BlockType.COMMAND,
-                        text: 'register user [USERNAME] with password [PASSWORD] and email [EMAIL]',
+                        text: 'register user [USERNAME] with password [PASSWORD] ',
                         arguments: {
                             USERNAME: {
                                 type: Scratch.ArgumentType.STRING,
@@ -65,10 +65,6 @@
                             PASSWORD: {
                                 type: Scratch.ArgumentType.STRING,
                                 defaultValue: 'password'
-                            },
-                            EMAIL: {
-                                type: Scratch.ArgumentType.STRING,
-                                defaultValue: 'email@example.com'
                             }
                         }
                     },
@@ -100,29 +96,6 @@
                                 type: Scratch.ArgumentType.STRING,
                                 menu: 'statusOptions',
                                 defaultValue: 'authenticated'
-                            }
-                        }
-                    },
-                    {
-                        blockType: Scratch.BlockType.LABEL,
-                        text: 'Email Actions'
-                    },
-                    {
-                        opcode: 'sendEmail',
-                        blockType: Scratch.BlockType.COMMAND,
-                        text: 'send email to [EMAIL] with subject [SUBJECT] and message [MESSAGE]',
-                        arguments: {
-                            EMAIL: {
-                                type: Scratch.ArgumentType.STRING,
-                                defaultValue: 'email@example.com'
-                            },
-                            SUBJECT: {
-                                type: Scratch.ArgumentType.STRING,
-                                defaultValue: 'Hello'
-                            },
-                            MESSAGE: {
-                                type: Scratch.ArgumentType.STRING,
-                                defaultValue: 'This is a message.'
                             }
                         }
                     },
@@ -192,7 +165,6 @@
         async registerUser(args) {
             const username = args.USERNAME;
             const password = args.PASSWORD;
-            const email = args.EMAIL;
 
             try {
                 const response = await Scratch.fetch('https://6741abede4647499008e694e.mockapi.io/turbowarp/API/ACCOUNT/accounts', {
@@ -200,15 +172,13 @@
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({ username, password, email })
+                    body: JSON.stringify({ username, password })
                 });
 
                 const data = await response.json();
                 if (response.ok) {
                     this.registeredUsers.add(username);
-                    this.setDebugMessage('Registration successful: ' + JSON.stringify(data));
-                    this.sendEmail({ EMAIL: email, SUBJECT: 'Registration Successful', MESSAGE: `Welcome, ${username}!` });
-                } else {
+                    this.setDebugMessage('Registration successful: ' + JSON.stringify
                     this.setDebugMessage('Registration failed: ' + JSON.stringify(data));
                 }
             } catch (error) {
@@ -247,32 +217,7 @@
             return false;
         }
 
-        async sendEmail(args) {
-            alert('This block is in development, not recommended to use');
-            const email = args.EMAIL;
-            const subject = args.SUBJECT;
-            const message = args.MESSAGE;
-
-            try {
-                const response = await Scratch.fetch('https://api.emailservice.com/send', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ email, subject, message })
-                });
-
-                const data = await response.json();
-                if (response.ok) {
-                    this.setDebugMessage('Email sent: ' + JSON.stringify(data));
-                } else {
-                    this.setDebugMessage('Failed to send email: ' + JSON.stringify(data));
-                }
-            } catch (error) {
-                this.setDebugMessage('Error sending email: ' + error.message);
-            }
-        }
-
+        
         useService(args) {
             const service = args.SERVICE;
             const action = args.ACTION;
