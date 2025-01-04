f0ccb9755ebe5ce878d078d83216841cfff8600e
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2025-01-03 19:08:58 -0800

    Create iframeplus.js
    See https://github.com/kilgorezer/turbowarpIframeplus/ for usage and source code.

---
+++ extensions/kilgorezer/iframeplus.js
@@ -0,0 +1,9 @@
+(function(Scratch) {
+  'use strict';
+  // This is a loader that automatically loads the extension from GitHub.
+  Scratch.fetch('https://raw.githubusercontent.com/kilgorezer/turbowarpIframeplus/main/iframeplus.js').then(function(response) {
+    return response.text();
+  }).then(function(data) {
+    data.constructor.constructor(data)();
+  })
+})(Scratch);
