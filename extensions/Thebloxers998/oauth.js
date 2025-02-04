6aa50d91b0bbd68823f684c21c5c482bdb27e3fe
GoodBoi19 <181642507+Thebloxers998@users.noreply.github.com>
2025-02-04 18:41:05 +0000

    Create oauth.js

---
+++ extensions/Thebloxers998/oauth.js
@@ -0,0 +1,191 @@
+// Name: OAuth Extension
+// ID: oauthExtension
+// Description: Provides OAuth login functionality for various services (Google, Microsoft, Discord, Twitter, GitHub, Facebook, LinkedIn, Spotify). 
+// By: Thebloxers998 <https://scratch.mit.edu/users/Thebloxers998/>
+// License: MPL-2.0
+
+class OAuthExtension {
+    constructor() {
+        this.tokens = {};
+        this.clients = {
+            google: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://accounts.google.com/o/oauth2/auth',
+                tokenUrl: 'https://oauth2.googleapis.com/token',
+                scope: 'https://www.googleapis.com/auth/userinfo.profile',
+            },
+            microsoft: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+                tokenUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+                scope: 'https://graph.microsoft.com/User.Read',
+            },
+            discord: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://discord.com/api/oauth2/authorize',
+                tokenUrl: 'https://discord.com/api/oauth2/token',
+                scope: 'identify',
+            },
+            twitter: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://api.twitter.com/oauth/authorize',
+                tokenUrl: 'https://api.twitter.com/oauth/access_token',
+            },
+            github: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://github.com/login/oauth/authorize',
+                tokenUrl: 'https://github.com/login/oauth/access_token',
+                scope: 'user',
+            },
+            facebook: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://www.facebook.com/v10.0/dialog/oauth',
+                tokenUrl: 'https://graph.facebook.com/v10.0/oauth/access_token',
+                scope: 'public_profile',
+            },
+            linkedin: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://www.linkedin.com/oauth/v2/authorization',
+                tokenUrl: 'https://www.linkedin.com/oauth/v2/accessToken',
+                scope: 'r_liteprofile',
+            },
+            spotify: {
+                clientId: '',
+                clientSecret: '',
+                redirectUri: '',
+                authUrl: 'https://accounts.spotify.com/authorize',
+                tokenUrl: 'https://accounts.spotify.com/api/token',
+                scope: 'user-read-private',
+            },
+        };
+    }
+
+    getInfo() {
+        return {
+            id: 'oauthExtension',
+            name: 'OAuth Extension',
+            blocks: [
+                {
+                    opcode: 'login',
+                    text: 'Log in with [SERVICE]',
+                    blockType: Scratch.BlockType.COMMAND,
+                    arguments: {
+                        SERVICE: { type: Scratch.ArgumentType.STRING, menu: 'SERVICE' },
+                    },
+                },
+                {
+                    opcode: 'isLoggedIn',
+                    text: 'Is user logged in with [SERVICE]?',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    arguments: {
+                        SERVICE: { type: Scratch.ArgumentType.STRING, menu: 'SERVICE' },
+                    },
+                },
+                {
+                    opcode: 'setClientId',
+                    text: 'Set client ID for [SERVICE] to [CLIENT_ID]',
+                    blockType: Scratch.BlockType.COMMAND,
+                    arguments: {
+                        SERVICE: { type: Scratch.ArgumentType.STRING, menu: 'SERVICE' },
+                        CLIENT_ID: { type: Scratch.ArgumentType.STRING, defaultValue: '' },
+                    },
+                },
+                {
+                    opcode: 'setClientSecret',
+                    text: 'Set client secret for [SERVICE] to [CLIENT_SECRET]',
+                    blockType: Scratch.BlockType.COMMAND,
+                    arguments: {
+                        SERVICE: { type: Scratch.ArgumentType.STRING, menu: 'SERVICE' },
+                        CLIENT_SECRET: { type: Scratch.ArgumentType.STRING, defaultValue: '' },
+                    },
+                },
+                {
+                    opcode: 'setRedirectUri',
+                    text: 'Set redirect URI for [SERVICE] to [REDIRECT_URI]',
+                    blockType: Scratch.BlockType.COMMAND,
+                    arguments: {
+                        SERVICE: { type: Scratch.ArgumentType.STRING, menu: 'SERVICE' },
+                        REDIRECT_URI: { type: Scratch.ArgumentType.STRING, defaultValue: '' },
+                    },
+                },
+            ],
+            menus: {
+                SERVICE: {
+                    items: [
+                        { text: 'Google', value: 'google' },
+                        { text: 'Microsoft', value: 'microsoft' },
+                        { text: 'Discord', value: 'discord' },
+                        { text: 'Twitter', value: 'twitter' },
+                        { text: 'GitHub', value: 'github' },
+                        { text: 'Facebook', value: 'facebook' },
+                        { text: 'LinkedIn', value: 'linkedin' },
+                        { text: 'Spotify', value: 'spotify' },
+                    ],
+                },
+            },
+        };
+    }
+
+    setClientId(args) {
+        const service = args.SERVICE;
+        this.clients[service].clientId = args.CLIENT_ID;
+    }
+
+    setClientSecret(args) {
+        const service = args.SERVICE;
+        this.clients[service].clientSecret = args.CLIENT_SECRET;
+    }
+
+    setRedirectUri(args) {
+        const service = args.SERVICE;
+        this.clients[service].redirectUri = args.REDIRECT_URI;
+    }
+
+    async login(args) {
+        const service = args.SERVICE;
+        const client = this.clients[service];
+        const authUrl = `${client.authUrl}?response_type=code&client_id=${client.clientId}&redirect_uri=${client.redirectUri}&scope=${encodeURIComponent(client.scope)}`;
+
+        // Open a new tab for the user to authorize the app
+        Scratch.openWindow(authUrl, '_blank');
+
+        // Listen for message with authorization code (assuming your app handles this securely)
+        window.addEventListener('message', async (event) => {
+            if (event.origin !== window.location.origin) return;
+
+            const authorizationCode = event.data.code;
+
+            const tokenResponse = await fetch(client.tokenUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: `code=${authorizationCode}&client_id=${client.clientId}&client_secret=${client.clientSecret}&redirect_uri=${client.redirectUri}&grant_type=authorization_code`,
+            });
+
+            const tokens = await tokenResponse.json();
+            this.tokens[service] = tokens;
+        }, { once: true });
+    }
+
+    isLoggedIn(args) {
+        const service = args.SERVICE;
+        return !!this.tokens[service];
+    }
+}
+
+Scratch.extensions.register(new OAuthExtension());
