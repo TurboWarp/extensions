3a0657e0250498dd2b18cf970d77a55a01972b5b
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2025-03-12 19:12:10 +0800

    will this work

--- extensions/Cheddarphanie/websockets-plus.js
+++ extensions/Cheddarphanie/websockets-plus.js
@@ -368,6 +368,7 @@
         console.log(`[WebSockets+] Attempting to connect to '${url}'..`);
 
       if (!this.fetchables[url])
+        // eslint-disable-next-line extension/check-can-fetch
         this.fetchables[url] = await Scratch.canFetch(url);
 
       if (!this.fetchables[url]) {
