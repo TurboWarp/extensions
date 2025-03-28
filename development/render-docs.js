95189ab859e60431243f51562dff00be424d0c5d
Brackets-Coder <titustedder@icloud.com>
2025-03-27 20:00:16 -0400

    Fix lint

--- development/render-docs.js
+++ development/render-docs.js
@@ -39,7 +39,6 @@ md.block.ruler.before(
     if (!match) return false;
 
     const type = match[1].toLowerCase();
-    let content = match[2].trim();
 
     const icons = {
       note: `<svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true" fill="#4493f8"><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"></path></svg>`,
