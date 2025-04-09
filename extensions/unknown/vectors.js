14cdeccd4b533bac2d491de37d205e99c108caac
unknown07724 <trash1.junk99@gmail.com>
2025-04-09 09:53:22 -0500

    Update vectors.js

--- extensions/unknown/vectors.js
+++ extensions/unknown/vectors.js
@@ -20,7 +20,7 @@
       return {
         id: "unknownvectors",
         name: Scratch.translate("Vectors"),
-        color1: "#275efe"
+        color1: "#275efe",
         blocks: [
           {
             opcode: "vectorCreate",
