2a29d6b4707424cd04d684c8e7e7955dff645b10
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-04-09 22:15:28 -0700

    Hyper-Sense.js -- Cast Issues

--- extensions/SharkPool/Hyper-Sense.js
+++ extensions/SharkPool/Hyper-Sense.js
@@ -4,7 +4,7 @@
 // By: SharkPool
 // License: MIT
 
-// Version 3.0.3 (TW Ver)
+// Version 3.0.31 (TW Ver)
 
 (function (Scratch) {
   "use strict";
@@ -36,13 +36,13 @@
           e.clientX, e.clientY, runtime.ioDevices.mouse.getScratchX(), runtime.ioDevices.mouse.getScratchY()
         ]
       });
-      window.addEventListener("keydown", (event) => {
-        const name = this.toProperKey(event.key, false);
+      window.addEventListener("keydown", (e) => {
+        const name = this.toProperKey(e.key, false);
         if (pressedKeys[name] === undefined) pressedKeys[name] = 0;
         curPressKey = name;
       });
-      window.addEventListener("keyup", (event) => {
-        delete pressedKeys[this.toProperKey(event.key, false)];
+      window.addEventListener("keyup", (e) => {
+        delete pressedKeys[this.toProperKey(e.key, false)];
         curPressKey = Object.keys(pressedKeys).pop() || "";
       });
     }
@@ -432,8 +432,8 @@
       if (mouse) { spriteNames.push({ text: "mouse-pointer", value: "_mouse_" }) }
       if (myself) { spriteNames.push({ text: "myself", value: "_myself_" }) }
       const targets = runtime.targets;
-      for (let index = 1; index < targets.length; index++) {
-        const target = targets[index];
+      for (let i = 1; i < targets.length; i++) {
+        const target = targets[i];
         if (target.isOriginal) {
           const targetName = target.getName();
           spriteNames.push({ text: targetName, value: targetName });
@@ -445,8 +445,8 @@
     handleScroll = (event) => {
       scrollDist += event.deltaY;
       oldScroll[1] = event.deltaY;
-      if (this.scrollWheelBool({ EVENT:"up", SECRET: true })) runtime.startHats("HyperSenseSP_scrollWheelHat", { EVENT: "up" });
-      if (this.scrollWheelBool({ EVENT:"down", SECRET: true })) runtime.startHats("HyperSenseSP_scrollWheelHat", { EVENT: "down" });
+      if (this.scrollWheelBool({ EVENT: "up", SECRET: true })) runtime.startHats("HyperSenseSP_scrollWheelHat", { EVENT: "up" });
+      if (this.scrollWheelBool({ EVENT: "down", SECRET: true })) runtime.startHats("HyperSenseSP_scrollWheelHat", { EVENT: "down" });
     };
 
     toProperKey(key, reverse) {
@@ -534,7 +534,7 @@
       const target = args.SPRITE1 === "_myself_" ? util.target : runtime.getSpriteTargetByName(args.SPRITE1);
       if (!target) return false;
       const oldDir = target.direction;
-      runtime.ext_scratch3_motion.pointTowards({ TOWARDS: args.SPRITE2 }, { ...util, target, ioQuery : util.ioQuery });
+      runtime.ext_scratch3_motion.pointTowards({ TOWARDS: args.SPRITE2 }, { ...util, target, ioQuery: util.ioQuery });
       const newDir = target.direction;
       target.setDirection(oldDir);
       return Math.round(newDir) === Math.round(oldDir);
@@ -580,8 +580,8 @@
       const list = [];
       const thisSprite = args.SPRITE === "_mouse_" ? "_mouse_" : args.SPRITE === "_myself_" ? util.target.getName() : args.SPRITE;
       const targets = runtime.targets;
-      for (let index = 1; index < targets.length; index++) {
-        const target = targets[index];
+      for (let i = 1; i < targets.length; i++) {
+        const target = targets[i];
         const name = `${target.getName()}${target.isOriginal ? "" : " (Clone)"}`;
         if (target.isTouchingObject(thisSprite) && name !== thisSprite) list.push(name);
       }
@@ -599,8 +599,8 @@
         pos = [nameTarget.x, nameTarget.y, nameTarget.id];
       }
       const targets = runtime.targets;
-      for (let index = 1; index < targets.length; index++) {
-        const target = targets[index];
+      for (let i = 1; i < targets.length; i++) {
+        const target = targets[i];
         const dx = pos[0] - target.x;
         const dy = pos[1] - target.y;
         if (Math.sqrt((dx * dx) + (dy * dy)) <= circ && target.id !== pos[2]) list.push(`${target.getName()}${target.isOriginal ? "" : " (Clone)"}`);
@@ -631,7 +631,7 @@
         case "special characters": {regex = /[A-Za-z0-9]/g; break }
         default: regex = /[^A-Za-z]/g;
       }
-      return args.STRING.replace(regex, "");
+      return Scratch.Cast.toString(args.STRING).replace(regex, "");
     }
 
     advancedAsk(args, util) {
@@ -644,7 +644,7 @@
         if (publicVars.askStuff) this.setAtt(publicVars.askStuff);
         if (publicVars.askType) this.setAskType(publicVars.askType);
         if (args.WAIT === "continue") resolve();
-        else runtime.once("ANSWER", () => { resolve() });
+        else runtime.once("ANSWER", () => resolve());
       });
     }
     advancedAskReporter(args, util) {
@@ -667,8 +667,9 @@
       let box = document.querySelector(`div[class*="question"]`);
       if (!box) return publicVars.askStuff = args;
       const canvas = getComputedStyle(render.canvas);
-      if (args.width) box.style.width = `${args.width * (parseInt(canvas.width) / 480)}px`;
-      const x = Scratch.Cast.toNumber(args.x) + (parseInt(canvas.width) / 2) - (args.width * (parseInt(canvas.width) / 480) / 2);
+      const width = Scratch.Cast.toNumber(args.width);
+      if (width) box.style.width = `${width * (parseInt(canvas.width) / 480)}px`;
+      const x = Scratch.Cast.toNumber(args.x) + (parseInt(canvas.width) / 2) - (width * (parseInt(canvas.width) / 480) / 2);
       const y = Scratch.Cast.toNumber(args.y) + (parseInt(canvas.height) / 2) - (askAs === "stage" ? 53 : 39);
       box.style.transform = `translate(${x}px, ${y * -1}px)`;
     }
@@ -705,7 +706,7 @@
     setAskType2(args, util) {
       try {
         const array = JSON.parse(args.TYPE);
-        if (array.length > 0) this.setAskType({...args, TYPE : "dropdown", LIST : array })
+        if (array.length > 0) this.setAskType({...args, TYPE: "dropdown", LIST: array })
       } catch {}
     }
 
