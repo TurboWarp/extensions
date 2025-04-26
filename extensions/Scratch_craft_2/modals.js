1cadd7503191edb13ab70cb21c0c344d176f867e
scratch-craft-2 <156839189+scratch-craft-2@users.noreply.github.com>
2025-04-26 23:19:58 +0300

    Update modals.js

--- extensions/Scratch_craft_2/modals.js
+++ extensions/Scratch_craft_2/modals.js
@@ -65,65 +65,7 @@
       }
       Confirm(args) {
         return confirm([args.text]);
-      }
-    }
-    Scratch.extensions.register(new YaGamesSDKExtension());
-  })(Scratch);Я",
-        );
-        return {
-          id: "htmlalert",
-          name: "modals",
-          color1: "#a3c0e1",
-          blocks: [
-            {
-              opcode: "alert",
-              blockType: Scratch.BlockType.COMMAND,
-              text: "alert [text]",
-              arguments: {
-                text: {
-                  defaultValue: "hello!",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           },
-          {
-              opcode: "Prompt",
-              blockType: Scratch.BlockType.REPORTER,
-              text: "prompt [text], default value: [classic]",
-              arguments: {
-                text: {
-                  defaultValue: "what's your name?",
-                  type: Scratch.ArgumentType.STRING,
-                },
-                  classic: {
-                  defaultValue: " ",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           },
-           { 
-              opcode: "Confirm",
-              blockType: Scratch.BlockType.BOOLEAN,
-              text: "confirm [text]",
-              arguments: {
-                text: {
-                  defaultValue: "Did you see that movie?",
-                  type: Scratch.ArgumentType.STRING,
-                },
-            },
-           },
-          ],
-        };
-      }
-      alert(args) {
-        alert([args.text]);
-      }
-      Prompt(args) {
-        return prompt([args.text], [args.classic]);
-      }
-      Confirm(args) {
-        return confirm([args.text]);
-      }
+      
     }
     Scratch.extensions.register(new YaGamesSDKExtension());
   })(Scratch);
