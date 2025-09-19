c35117d68a381dc12d48a478db39b536d92bebd9
ampelectrecuted <197376797+ampelectrecuted@users.noreply.github.com>
2025-09-19 19:01:48 +0100

    copy from deleted fork

---
+++ extensions/8to16/oscillators.js
@@ -0,0 +1,87 @@
+// Name: Oscillators
+// ID: aecOscillator
+// Description: Synthensize simple sound waves.
+// By: 8to16 <https://scratch.mit.edu/users/8to16/>
+// License: MPL-2.0
+
+(function (Scratch) {
+  "use strict";
+  let context = new AudioContext();
+  let oscillator = new OscillatorNode(context);
+  oscillator.connect(context.destination);
+  let isStart = false;
+  let isPaused = true;
+
+  class OscillatorExtension {
+    getInfo() {
+      return {
+        id: "aecOscillator",
+        name: Scratch.translate("Oscillators"),
+        blocks: [
+          {
+            opcode: "startFreq",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("start [WAVEFORM] wave at [FREQ] Hz"),
+            arguments: {
+              WAVEFORM: {
+                type: Scratch.ArgumentType.STRING,
+                menu: "WAVEFORM_MENU",
+              },
+              FREQ: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 440,
+              },
+            },
+          },
+          {
+            opcode: "stopFreq",
+            blockType: Scratch.BlockType.COMMAND,
+            text: Scratch.translate("stop wave"),
+          },
+        ],
+        menus: {
+          WAVEFORM_MENU: {
+            acceptReporters: true,
+            items: [
+              {
+                text: Scratch.translate("sine"),
+                value: "sine",
+              },
+              {
+                text: Scratch.translate("square"),
+                value: "square",
+              },
+              {
+                text: Scratch.translate("sawtooth"),
+                value: "sawtooth",
+              },
+              {
+                text: Scratch.translate("triangle"),
+                value: "triangle",
+              },
+            ],
+          },
+        },
+      };
+    }
+
+    startFreq(args) {
+      oscillator.type = Scratch.Cast.toString(args.WAVEFORM);
+      oscillator.frequency.setValueAtTime(Scratch.Cast.toNumber(args.FREQ), context.currentTime);
+      if (!isStart) {
+        oscillator.start();
+        isStart = true;
+      }
+      if (isPaused) {
+        context.resume();
+        isPaused = false;
+      }
+    }
+
+    stopFreq() {
+      context.suspend();
+      isPaused = true;
+    }
+  }
+  Scratch.extensions.register(new OscillatorExtension());
+})(Scratch);
