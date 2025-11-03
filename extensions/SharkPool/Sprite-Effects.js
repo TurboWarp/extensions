1cae8e993d27f95d63a5d00c982b123097ddad95
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-11-02 17:50:14 -0800

    fix duplicate filter bug

--- extensions/SharkPool/Sprite-Effects.js
+++ extensions/SharkPool/Sprite-Effects.js
@@ -4,7 +4,7 @@
 // By: SharkPool
 // License: MIT
 
-// Version V.1.0.0
+// Version V.1.0.01
 
 (function (Scratch) {
   "use strict";
@@ -194,7 +194,7 @@
     cssString = cssString.trim();
 
     const oldCssString = canvas.style.filter;
-    if (oldCssString.includes(cssString)) {
+    if (oldCssString.includes(canvasFilterString)) {
       canvas.style.filter = oldCssString.replace(canvasFilterString, cssString);
     } else {
       canvas.style.filter += cssString;
