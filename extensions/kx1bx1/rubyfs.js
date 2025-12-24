2a3727b507f9b5a0e58c102ae4d6adba9f7efc7c
kx1bx1 <flamboyance@atomicmail.io>
2025-12-24 16:18:49 -0600

    hotfix: no description punctuation

--- extensions/kx1bx1/rubyfs.js
+++ extensions/kx1bx1/rubyfs.js
@@ -1,6 +1,6 @@
 // Name: RubyFS
 // ID: rubyFS
-// Description: A structured, in-memory file system for Scratch projects. (Consolidated Version)
+// Description: A structured, in-memory file system for Scratch projects.
 // By: kx1bx1 <https://scratch.mit.edu/users/kx1bx1/>
 // Original: 0832 <https://scratch.mit.edu/users/0832/>
 // License: MIT
