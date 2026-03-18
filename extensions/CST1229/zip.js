7358fae4ab44892d85bc0308c5e18358bf6e1ddf
CST1229 <68464103+CST1229@users.noreply.github.com>
2026-03-18 21:41:00 +0100

    CST1229/zip: relicense to MPL
    should have done this a while ago

--- extensions/CST1229/zip.js
+++ extensions/CST1229/zip.js
@@ -2,7 +2,7 @@
 // ID: cst1229zip
 // Description: Create and edit .zip format files, including .sb3 files.
 // By: CST1229 <https://scratch.mit.edu/users/CST1229/>
-// License: MIT AND LGPL-3.0
+// License: MIT AND MPL-2.0
 
 (function (Scratch) {
   "use strict";
