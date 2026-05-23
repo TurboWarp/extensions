ac4ce0b5f37a2ac298d1214e0784327568bda64c
Thomas Weber <muffin@muffin.ink>
2026-05-23 14:14:08 -0500

    Completely rewrite skin management

--- extensions/lab/video-sprite.js
+++ extensions/lab/video-sprite.js
@@ -11,13 +11,48 @@
     throw new Error("Video Sprites must run unsandboxed.");
   }
 
-  const MIN_ZOOM = 20;
-  const MAX_ZOOM = 2000;
+  const runtime = Scratch.vm.runtime;
+  const renderer = runtime.renderer;
 
-  // TODO: why are we handling Infinity differently? that's just clearly wrong. and NaN shouldn't be possible.
-  const clampZoom = (value) => {
-    const zoom = Number.isFinite(value) ? value : 100;
-    return Math.max(MIN_ZOOM, Math.min(MAX_ZOOM, zoom));
+  const Skin = renderer.exports.Skin;
+  const twgl = renderer.exports.twgl;
+  const gl = renderer.gl;
+  const videoDevice = runtime.ioDevices.video;
+
+  const workCanvas = document.createElement("canvas");
+  const workContext = workCanvas.getContext("2d", {
+    willReadFrequently: true
+  });
+
+  // TODO: zoom should be per-target, not hardcoded
+  const sampleVideo = (width, height) => {
+    const sourceCanvas = videoDevice.getFrame({ format: "canvas" });
+    if (!sourceCanvas) {
+      return null;
+    }
+
+    const videoW = sourceCanvas.width;
+    const videoH = sourceCanvas.height;
+    const upscaleFactor = width > height ? videoW / width : videoH / height;
+    const cropWidth = width * upscaleFactor;
+    const cropHeight = height * upscaleFactor;
+    const cropX = (videoW - cropWidth) / 2;
+    const cropY = (videoH - cropHeight) / 2;
+
+    workContext.clearRect(0, 0, width, height);
+    workContext.drawImage(
+      sourceCanvas,
+      cropX,
+      cropY,
+      cropWidth,
+      cropHeight,
+      0,
+      0,
+      width,
+      height
+    );
+
+    return workContext.getImageData(0, 0, width, height).data;
   };
 
   // TODO: this is wrong, should be MUCH less tolerant
@@ -32,76 +67,132 @@
       Math.abs(b - color.b) <= tolerance;
   };
 
-  // TODO: remove - should not be needed
-  const getCurrentCostume = (target) =>
-    target.sprite.costumes[target.currentCostume] || {};
+  class VideoSpriteSkin extends Skin {
+    constructor(id, target) {
+      super(id, renderer);
 
-  // TODO: remove
-  const getBaseImageSize = (image) => ({
-    width: image.naturalWidth || image.videoWidth || image.width || 0,
-    height: image.naturalHeight || image.videoHeight || image.height || 0
-  });
+      // Assumed to always contain webcam feed
+      this.private = true;
 
-  // TODO: remove - should not be needed
-  const getDrawableSkinId = (drawable) => {
-    if (!drawable) {
-      return undefined;
-    }
-    if (typeof drawable.skin === "object" && drawable.skin && typeof drawable.skin.id === "number") {
-      return drawable.skin.id;
-    }
-    if (typeof drawable._skinId === "number") {
-      return drawable._skinId;
-    }
-    if (typeof drawable.skinId === "number") {
-      return drawable.skinId;
-    }
-    return undefined;
-  };
+      this.target = target;
 
-  const getTargetBaseSkinId = (target, drawable) => {
-    const costume = getCurrentCostume(target);
-    if (typeof costume.skinId === "number") {
-      return costume.skinId;
+      this.mode = "mask";
+      this.color = null;
     }
-    const drawableSkinId = getDrawableSkinId(drawable);
-    const generatedSkinId = target.__videoSpritesSkinId;
-    if (drawableSkinId !== undefined && drawableSkinId !== generatedSkinId) {
-      return drawableSkinId;
-    }
-    return target.__videoSpritesOriginalSkinId;
-  };
 
-  const rememberOriginalSkin = (target, drawable, baseSkinId) => {
-    if (target.__videoSpritesOriginalSkinId !== undefined) {
-      return;
+    dispose() {
+      if (this._texture) {
+        gl.deleteTexture(this._texture);
+        this._texture = null;
+      }
+      super.dispose();
     }
-    const currentSkinId = getDrawableSkinId(drawable);
-    target.__videoSpritesOriginalSkinId = currentSkinId !== undefined ? currentSkinId : baseSkinId;
-  };
+
+    _baseSkin() {
+      const costume = this.target.sprite.costumes[this.target.currentCostume];
+      if (!costume || typeof costume.skinId !== "number") {
+        return null;
+      }
+      return renderer._allSkins[costume.skinId] || null;
+    }
+
+    get size() {
+      const base = this._baseSkin();
+      return base ? base.size : [0, 0];
+    }
+
+    get rotationCenter() {
+      const base = this._baseSkin();
+      return base ? base.rotationCenter : this._rotationCenter;
+    }
+
+    useNearest() {
+      return false;
+    }
+
+    getTexture(scale) {
+      this._render();
+      return this._texture || super.getTexture(scale);
+    }
+
+    updateSilhouette() {
+      this._render();
+      this._silhouette.unlazy();
+    }
+
+    _render() {
+      const baseSkin = this._baseSkin();
+      if (!baseSkin) {
+        return;
+      }
+
+      baseSkin.updateSilhouette();
+      const silhouette = baseSkin._silhouette;
+      const silhouetteData = silhouette._colorData;
+      const width = silhouette._width;
+      const height = silhouette._height;
+      if (!width || !height || !silhouetteData) {
+        return;
+      }
+
+      if (workCanvas.width !== width || workCanvas.height !== height) {
+        workCanvas.width = width;
+        workCanvas.height = height;
+      }
+
+      const sourceData = sampleVideo(width, height);
+      if (!sourceData) {
+        return;
+      }
+
+      const out = new Uint8ClampedArray(silhouetteData);
+      const color = this.color;
+      const isMask = this.mode === "mask";
+
+      for (let i = 0; i < out.length; i += 4) {
+        const alpha = out[i + 3];
+        if (alpha === 0) {
+          continue;
+        }
+
+        const shouldFill = isMask
+          ? true
+          : colorMatches(out[i], out[i + 1], out[i + 2], alpha, color);
+
+        if (shouldFill) {
+          out[i] = sourceData[i];
+          out[i + 1] = sourceData[i + 1];
+          out[i + 2] = sourceData[i + 2];
+        }
+      }
+
+      workContext.putImageData(new ImageData(out, width, height), 0, 0);
+
+      if (!this._texture) {
+        this._texture = twgl.createTexture(gl, {
+          auto: false,
+          wrap: gl.CLAMP_TO_EDGE
+        });
+      }
+      this._setTexture(workCanvas);
+    }
+  }
 
   class VideoSpritesExtension {
     constructor() {
-      this.vm = Scratch.vm;
-      this.runtime = this.vm.runtime;
-      this.renderer = this.runtime.renderer;
-      this.videoDevice = this.runtime.ioDevices.video;
-
-      this.workCanvas = document.createElement("canvas");
-      this.workContext = this.workCanvas.getContext("2d", {
-        willReadFrequently: true
-      });
-
-      this.zoom = 100;
-      this.renderHandle = null;
-      this.isRendering = false;
       this.fillStates = new Map();
       this._hidPreview = false;
 
-      const _boundRefresh = this.refreshAllTargets.bind(this);
-      this.runtime.on("PROJECT_STOP_ALL", _boundRefresh);
-      this.runtime.on("PROJECT_LOADED", _boundRefresh);
-      this.runtime.on("PROJECT_CHANGED", _boundRefresh);
+      runtime.on("BEFORE_EXECUTE", () => this._onBeforeExecute());
+      runtime.on("PROJECT_STOP_ALL", () => this._disposeAll());
+      runtime.on("PROJECT_LOADED", () => this._disposeAll());
+      runtime.on("targetWasRemoved", (target) => {
+        const skin = this.fillStates.get(target.id);
+        if (skin) {
+          renderer.destroySkin(skin.id);
+          this.fillStates.delete(target.id);
+        }
+      });
     }
 
     getInfo() {
@@ -159,66 +250,47 @@
     }
 
     async videoSpriteFillSprite(args, util) {
-      const target = util.target;
-
       if (!(await this.ensureCamera())) {
         return;
       }
-
-      this.fillStates.set(target.id, {
-        mode: "mask",
-        color: null
-      });
-
-      this.startRenderLoop();
-      this.updateTargetSkin(target);
+      const skin = this._getOrCreateSkin(util.target);
+      skin.mode = "mask";
+      skin.color = null;
+      this._attach(util.target, skin);
     }
 
     async videoSpriteFillColor(args, util) {
-      const target = util.target;
-
       if (!(await this.ensureCamera())) {
         return;
       }
-
-      this.fillStates.set(target.id, {
-        mode: "color",
-        color: Scratch.Cast.toRgbColorObject(args.COLOR)
-      });
-
-      this.startRenderLoop();
-      this.updateTargetSkin(target);
+      const skin = this._getOrCreateSkin(util.target);
+      skin.mode = "color";
+      skin.color = Scratch.Cast.toRgbColorObject(args.COLOR);
+      this._attach(util.target, skin);
     }
 
-    changeCameraBy(args) {
-      const amount = Scratch.Cast.toNumber(args.CAMERA_SCALE_INC);
-      this.zoom = clampZoom(this.zoom + amount);
-      this.refreshAllTargets();
-    }
+    // TODO: zoom needs to be per-target rather than global. Hardcoded for now.
+    changeCameraBy() {}
 
-    scaleCamera(args) {
-      this.zoom = clampZoom(Scratch.Cast.toNumber(args.CAMERA_SCALE));
-      this.refreshAllTargets();
-    }
+    scaleCamera() {}
 
     videoSpriteOff(args, util) {
-      this.restoreTarget(util.target);
-
-      if (this.fillStates.size === 0) {
-        this.stopRenderLoop();
+      const target = util.target;
+      const skin = this.fillStates.get(target.id);
+      if (!skin) {
+        return;
       }
-    }
-
-    cameraReady() {
-      return !!this.videoDevice.getFrame({ format: "canvas" });
+      target.setCostume(target.currentCostume);
+      renderer.destroySkin(skin.id);
+      this.fillStates.delete(target.id);
     }
 
     async ensureCamera() {
       try {
-        await this.videoDevice.enableVideo();
+        await videoDevice.enableVideo();
         if (!this._hidPreview) {
           this._hidPreview = true;
-          this.videoDevice.setPreviewGhost(100);
+          videoDevice.setPreviewGhost(100);
         }
         return true;
       } catch (error) {
@@ -226,271 +298,54 @@
       }
     }
 
-    startRenderLoop() {
-      if (this.renderHandle) {
+    _getOrCreateSkin(target) {
+      const existing = this.fillStates.get(target.id);
+      if (existing) {
+        return existing;
+      }
+      const id = renderer._nextSkinId++;
+      const skin = new VideoSpriteSkin(id, target);
+      renderer._allSkins[id] = skin;
+      this.fillStates.set(target.id, skin);
+      return skin;
+    }
+
+    _attach(target, skin) {
+      renderer.updateDrawableSkinId(target.drawableID, skin.id);
+      skin.emitWasAltered();
+      runtime.requestRedraw();
+    }
+
+    _onBeforeExecute() {
+      if (this.fillStates.size === 0) {
         return;
       }
-
-      const tick = async () => {
-        this.renderHandle = null;
-
-        if (this.fillStates.size === 0) {
-          return;
-        }
-
-        if (this.cameraReady() && !this.isRendering) {
-          this.isRendering = true;
-
-          try {
-            for (const target of this.getTargetsForFillStates()) {
-              this.updateTargetSkin(target);
-            }
-          } finally {
-            this.isRendering = false;
-          }
-        }
-
-        this.renderHandle = requestAnimationFrame(tick);
-      };
-
-      this.renderHandle = requestAnimationFrame(tick);
-    }
-
-    stopRenderLoop() {
-      if (this.renderHandle) {
-        cancelAnimationFrame(this.renderHandle);
-        this.renderHandle = null;
-      }
-    }
-
-    refreshAllTargets() {
-      for (const target of this.getTargetsForFillStates()) {
-        this.updateTargetSkin(target);
-      }
-    }
-
-    getTargetsForFillStates() {
-      const targets = [];
-
-      for (const targetId of this.fillStates.keys()) {
-        const target = this.runtime.getTargetById(targetId);
+      for (const [targetId, skin] of this.fillStates) {
+        const target = runtime.getTargetById(targetId);
         if (!target) {
+          renderer.destroySkin(skin.id);
           this.fillStates.delete(targetId);
           continue;
         }
-
-        targets.push(target);
-      }
-
-      if (this.fillStates.size === 0) {
-        this.stopRenderLoop();
-      }
-
-      return targets;
-    }
-    updateTargetSkin(target) {
-      const fillState = this.fillStates.get(target.id);
-      if (!fillState || !this.renderer) {
-        return;
-      }
-
-      const drawable = this.getDrawable(target);
-      const baseSkinId = getTargetBaseSkinId(target, drawable);
-      const baseImage = this.getSkinImage(baseSkinId);
-
-      if (!baseImage) {
-        return;
-      }
-
-      const size = getBaseImageSize(baseImage);
-      const width = size.width;
-      const height = size.height;
-
-      if (!width || !height) {
-        return;
-      }
-
-      if (this.workCanvas.width !== width || this.workCanvas.height !== height) {
-        this.workCanvas.width = width;
-        this.workCanvas.height = height;
-      }
-
-      this.workContext.clearRect(0, 0, width, height);
-      this.workContext.drawImage(baseImage, 0, 0, width, height);
-
-      let maskImageData;
-      try {
-        maskImageData = this.workContext.getImageData(0, 0, width, height);
-      } catch (error) {
-        return;
-      }
-
-      const source = this.sampleVideoToSize(width, height);
-      if (!source) {
-        return;
-      }
-      const maskData = maskImageData.data;
-      const sourceData = source.data;
-      const color = fillState.color;
-
-      for (let i = 0; i < maskData.length; i += 4) {
-        const alpha = maskData[i + 3];
-        if (alpha === 0) {
-          maskData[i + 3] = 0;
-          continue;
+        const drawable = renderer._allDrawables[target.drawableID];
+        if (drawable && drawable.skin !== skin) {
+          renderer.updateDrawableSkinId(target.drawableID, skin.id);
         }
+        skin.emitWasAltered();
+      }
+    }
 
-        const shouldFill = fillState.mode === "mask"
-          ? true
-          : colorMatches(maskData[i], maskData[i + 1], maskData[i + 2], alpha, color);
-
-        if (shouldFill) {
-          maskData[i] = sourceData[i];
-          maskData[i + 1] = sourceData[i + 1];
-          maskData[i + 2] = sourceData[i + 2];
-          maskData[i + 3] = alpha;
+    _disposeAll() {
+      for (const [targetId, skin] of this.fillStates) {
+        const target = runtime.getTargetById(targetId);
+        if (target) {
+          target.setCostume(target.currentCostume);
         }
+        renderer.destroySkin(skin.id);
       }
-
-      this.workContext.putImageData(maskImageData, 0, 0);
-
-      const fallbackCenter = [width / 2, height / 2];
-      const costume = getCurrentCostume(target);
-      const costumeCenter = (typeof costume.rotationCenterX === "number" && typeof costume.rotationCenterY === "number")
-        ? [costume.rotationCenterX, costume.rotationCenterY]
-        : null;
-      const rotationCenter = this.getRotationCenter(baseSkinId) || costumeCenter || fallbackCenter;
-
-      let generatedSkinId = target.__videoSpritesSkinId;
-
-      if (generatedSkinId === undefined || !this.renderer._allSkins[generatedSkinId]) {
-        generatedSkinId = this.renderer.createBitmapSkin(this.workCanvas, 1, rotationCenter);
-        target.__videoSpritesSkinId = generatedSkinId;
-      } else {
-        this.renderer.updateBitmapSkin(generatedSkinId, this.workCanvas, 1, rotationCenter);
-      }
-
-      rememberOriginalSkin(target, drawable, baseSkinId);
-      this.renderer.updateDrawableSkinId(target.drawableID, generatedSkinId);
-
-      if (this.runtime.requestRedraw) {
-        this.runtime.requestRedraw();
-      }
+      this.fillStates.clear();
     }
-
-    sampleVideoToSize(width, height) {
-      const sourceCanvas = this.videoDevice.getFrame({ format: "canvas" });
-      if (!sourceCanvas) {
-        return null;
-      }
-
-      const videoW = sourceCanvas.width;
-      const videoH = sourceCanvas.height;
-      const upscaleFactor = (100 / this.zoom) *
-        (width > height ? videoW / width : videoH / height);
-      const cropWidth = width * upscaleFactor;
-      const cropHeight = height * upscaleFactor;
-      const cropX = (videoW - cropWidth) / 2;
-      const cropY = (videoH - cropHeight) / 2;
-
-      this.workContext.clearRect(0, 0, width, height);
-      this.workContext.drawImage(
-        sourceCanvas,
-        cropX,
-        cropY,
-        cropWidth,
-        cropHeight,
-        0,
-        0,
-        width,
-        height
-      );
-
-      return this.workContext.getImageData(0, 0, width, height);
-    }
-
-    restoreTarget(target) {
-      this.fillStates.delete(target.id);
-
-      const costume = getCurrentCostume(target);
-      const restoreSkinId = typeof costume.skinId === "number"
-        ? costume.skinId
-        : target.__videoSpritesOriginalSkinId;
-      if (restoreSkinId !== undefined && this.renderer && target.drawableID !== undefined) {
-        this.renderer.updateDrawableSkinId(target.drawableID, restoreSkinId);
-      }
-
-      const generatedSkinId = target.__videoSpritesSkinId;
-      if (generatedSkinId !== undefined && this.renderer && this.renderer._allSkins[generatedSkinId]) {
-        this.renderer.destroySkin(generatedSkinId);
-      }
-
-      delete target.__videoSpritesSkinId;
-      delete target.__videoSpritesOriginalSkinId;
-
-      if (this.runtime.requestRedraw) {
-        this.runtime.requestRedraw();
-      }
-    }
-
-    getDrawable(target) {
-      return this.renderer && this.renderer._allDrawables
-        ? this.renderer._allDrawables[target.drawableID]
-        : null;
-    }
-
-    getSkinImage(skinId) {
-      if (typeof skinId !== "number" || !this.renderer || !this.renderer._allSkins) {
-        return null;
-      }
-
-      const skin = this.renderer._allSkins[skinId];
-      if (!skin) {
-        return null;
-      }
-
-      if (skin._svgImage) {
-        return skin._svgImage;
-      }
-
-      if (skin._bitmapData) {
-        return skin._bitmapData;
-      }
-
-      if (skin._canvas) {
-        return skin._canvas;
-      }
-
-      if (skin._texture && skin._texture.canvas) {
-        return skin._texture.canvas;
-      }
-
-      return null;
-    }
-
-    getRotationCenter(skinId) {
-      if (typeof skinId !== "number" || !this.renderer || !this.renderer._allSkins) {
-        return null;
-      }
-
-      const skin = this.renderer._allSkins[skinId];
-      if (!skin) {
-        return null;
-      }
-
-      if (Array.isArray(skin.rotationCenter)) {
-        return skin.rotationCenter;
-      }
-
-      if (Array.isArray(skin._rotationCenter)) {
-        return skin._rotationCenter;
-      }
-
-      return null;
-    }
-
   }
 
   Scratch.extensions.register(new VideoSpritesExtension());
 })(Scratch);
-
