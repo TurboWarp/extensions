1467169b40c643cee65f0cf7d9ae7288c0061e5e
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-07-29 13:26:51 -0700

    dont cache images when we forcefully change the canvas size

--- extensions/SharkPool/Image-Effects.js
+++ extensions/SharkPool/Image-Effects.js
@@ -1583,6 +1583,7 @@
       const { canvas, context } = ImageHelper.getHelper();
       ImageHelper.prepCanvas(image);
 
+      ImageHelper.currentImageHash = null; // we change the canvas size here, dont cache
       const canvasSize = Math.max(image.width, image.height) * 2;
       canvas.width = canvasSize;
       canvas.height = canvasSize;
